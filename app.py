2115775d9780191c581f86fed578df3646ddc3b5
Ben Adida <ben@adida.net>
2019-06-12 04:38:25 +0000

    more models, beginning of API skeleton

--- app.py
+++ app.py
@@ -11,6 +11,45 @@ db = SQLAlchemy(app)
 
 from models import *
 
+# get/set audit config
+# state and jurisdictions[]
+@app.route('/admin/config')
+def audit_config():
+    pass
+
+@app.route('/admin/setup')
+def audit_setup():
+    pass
+
+@app.route('/admin/randomseed')
+def audit_randomseed():
+    pass
+
+# state of all the jurisdictions, round #, and contest status
+@app.route('/admin/status')
+def audit_status():
+    pass
+
+@app.route('/jurisdiction/<jurisdiction_id>/manifest')
+def jurisdiction_manifest(jurisdiction_id):
+    pass
+
+@app.route('/jurisdiction/<jurisdiction_id>/auditboards')
+def jurisdiction_auditboards(jurisdiction_id):
+    pass
+
+@app.route('/jurisdiction/<jurisdiction_id>/auditboard/<audit_board_id>')
+def jurisdiction_auditboard(jurisdiction_id, audit_board_id):
+    pass
+
+@app.route('/jurisdiction/<jurisdiction_id>/auditboard/<audit_board_id>/status')
+def jurisdiction_auditboard_status(jurisdiction_id, audit_board_id):
+    pass
+
+@app.route('/jurisdiction/<jurisdiction_id>/auditboard/<audit_board_id>/cvr/<cvr_id>')
+def jurisdiction_auditboard_cvr(jurisdiction_id, audit_board_id, cvr_id):
+    pass
+
 
 
 # React App
