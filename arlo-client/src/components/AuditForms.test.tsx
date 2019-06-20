be49833836537025fd16707599147bc369ec7eb4
morgan <morgan.j.love@gmail.com>
2019-06-20 14:07:11 -0700

    Add snapshot tests for AudiForms

---
+++ arlo-client/src/components/AuditForms.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import AuditForms from './AuditForms';
+
+it('renders corretly', () => {
+    const { container } = render(<AuditForms />)
+    expect(container).toMatchSnapshot()
+});
