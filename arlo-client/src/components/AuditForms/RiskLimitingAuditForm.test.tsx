e5147b509cbee6de079ffc23210d3a0e17204c4f
morgan <morgan.j.love@gmail.com>
2019-07-27 19:02:10 -0700

    wrapping AuditForms tests in act()

--- arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
+++ arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 import AuditForms from './RiskLimitingAuditForm'
 import apiMock from '../utilities'
 
 jest.mock('../utilities')
 
 it('renders correctly', () => {
-  const { container } = render(<AuditForms />)
+  let utils: any
+  act(() => {
+    utils = render(<AuditForms />)
+  })
+  const { container } = utils
   expect(container).toMatchSnapshot()
 })
 
 it('fetches from api as expected', () => {
-  render(<AuditForms />)
+  act(() => {
+    render(<AuditForms />)
+  })
   expect(apiMock).toBeCalled()
   expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
 })
