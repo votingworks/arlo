211dbf21341caa5d53fa6bbd8ea45efdc14acb33
morgan <morgan.j.love@gmail.com>
2019-07-31 14:56:01 -0700

    updating snapshots

--- arlo-client/src/components/Header.test.tsx
+++ arlo-client/src/components/Header.test.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import Header from './Header'
 
-it('renders correctly', () => {
+it('renders correctly at /home', () => {
+  const { container, queryByText } = render(<Header isHome />)
+  expect(container).toMatchSnapshot()
+  expect(queryByText('Clear & Restart')).toBeFalsy()
+  expect(container).toMatchSnapshot()
+})
+
+it('renders correctly at /election', () => {
   const { container, getByText } = render(<Header />)
   expect(container).toMatchSnapshot()
   fireEvent.click(getByText('Clear & Restart'))
