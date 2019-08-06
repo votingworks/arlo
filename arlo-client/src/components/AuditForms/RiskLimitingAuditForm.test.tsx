512e6ca91bf370c557369612bc2c8632c1c3d5f0
morgan <morgan.j.love@gmail.com>
2019-08-05 17:15:25 -0700

    adding describe block to RiskLimitingAuditForm

--- arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
+++ arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
@@ -7,19 +7,21 @@ import statusStates from './_mocks'
 jest.mock('../utilities')
 ;(apiMock as jest.Mock).mockImplementation(() => statusStates[0])
 
-it('renders correctly', () => {
-  let utils: any
-  act(() => {
-    utils = render(<AuditForms />)
+describe('RiskLimitingAuditForm', () => {
+  it('renders correctly', () => {
+    let utils: any
+    act(() => {
+      utils = render(<AuditForms />)
+    })
+    const { container } = utils
+    expect(container).toMatchSnapshot()
   })
-  const { container } = utils
-  expect(container).toMatchSnapshot()
-})
 
-it('fetches from api as expected', () => {
-  act(() => {
-    render(<AuditForms />)
+  it('fetches from api as expected', () => {
+    act(() => {
+      render(<AuditForms />)
+    })
+    expect(apiMock).toBeCalled()
+    expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
   })
-  expect(apiMock).toBeCalled()
-  expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
 })
