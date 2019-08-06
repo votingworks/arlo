fe2b233447c6f323c094bf88cb3ab3b9313d36fb
morgan <morgan.j.love@gmail.com>
2019-08-05 17:38:58 -0700

    getting to 100% coverage on RiskLimitingAuditForm

--- arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
+++ arlo-client/src/components/AuditForms/RiskLimitingAuditForm.test.tsx
@@ -5,23 +5,58 @@ import apiMock from '../utilities'
 import statusStates from './_mocks'
 
 jest.mock('../utilities')
-;(apiMock as jest.Mock).mockImplementation(() => statusStates[0])
+;(apiMock as jest.Mock)
+  .mockImplementationOnce(() => statusStates[0])
+  .mockImplementationOnce(() => statusStates[1])
+  .mockImplementationOnce(() => statusStates[2])
+  .mockImplementationOnce(() => statusStates[3])
 
 describe('RiskLimitingAuditForm', () => {
-  it('renders correctly', () => {
+  it('renders correctly and fetches initial state from api', () => {
     let utils: any
     act(() => {
       utils = render(<AuditForms />)
     })
     const { container } = utils
+
+    expect(container).toMatchSnapshot()
+    expect(apiMock).toBeCalledTimes(1)
+    expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
+  })
+
+  it('renders SelectBallotsToAudit when /audit/status returns contest data', () => {
+    let utils: any
+    act(() => {
+      utils = render(<AuditForms />)
+    })
+    const { container } = utils
+
+    expect(container).toMatchSnapshot()
+    expect(apiMock).toBeCalledTimes(2)
+    expect((apiMock as jest.Mock).mock.calls[1][0]).toBe('/audit/status')
+  })
+
+  it('does not render CalculateRiskMeasurement when audit.jurisdictions has length but audit.rounds does not', () => {
+    let utils: any
+    act(() => {
+      utils = render(<AuditForms />) // this one will not have the first empty round
+    })
+    const { container } = utils
+
+    expect(apiMock).toBeCalledTimes(3)
+    expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
     expect(container).toMatchSnapshot()
   })
 
-  it('fetches from api as expected', () => {
+  it('renders CalculateRiskMeasurement when /audit/status returns round data', () => {
+    let utils: any
     act(() => {
-      render(<AuditForms />)
+      utils = render(<AuditForms />) // this one will not have the first empty round
     })
-    expect(apiMock).toBeCalled()
+    const { container } = utils
+
+    expect(apiMock).toBeCalledTimes(4)
     expect((apiMock as jest.Mock).mock.calls[0][0]).toBe('/audit/status')
+    expect(container).toMatchSnapshot()
   })
 })
