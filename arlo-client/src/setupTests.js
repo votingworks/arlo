e7b4bc03881d80a7c5932adc55082bcfdd09ca54
morgan <morgan.j.love@gmail.com>
2019-08-12 11:57:19 -0700

    removing enzyme

--- arlo-client/src/setupTests.js
+++ arlo-client/src/setupTests.js
@@ -1,7 +1,4 @@
 // test-setup.js
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+
 import '@testing-library/jest-dom/extend-expect'
 import '@testing-library/react/cleanup-after-each'
-
-configure({ adapter: new Adapter() })
