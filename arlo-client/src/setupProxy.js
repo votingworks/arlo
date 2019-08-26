017f45920119fcbac3a9642e321c289828f8e5ee
morgan <morgan.j.love@gmail.com>
2019-08-26 09:46:14 -0700

    adding /election to setupProxy

--- arlo-client/src/setupProxy.js
+++ arlo-client/src/setupProxy.js
@@ -11,6 +11,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use(proxy('/admin', { target: 'http://localhost:3001/' }))
+  app.use(proxy('/election', { target: 'http://localhost:3001/' }))
   app.use(proxy('/audit', { target: 'http://localhost:3001/' }))
   app.use(proxy('/jurisdiction', { target: 'http://localhost:3001/' }))
 }
