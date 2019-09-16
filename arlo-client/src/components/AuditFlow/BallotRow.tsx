b613f906f4bb42ed3438fe0e45ed6a4157a7793d
morgan <morgan.j.love@gmail.com>
2019-09-16 15:19:08 -0700

    adding BallotRow component

---
+++ arlo-client/src/components/AuditFlow/BallotRow.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const BallotRow = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin: 20px 0;
+
+  .ballot-side {
+    width: 200px;
+    padding: 20px 0;
+  }
+  .ballot-main {
+    width: 50%;
+    padding: 20px;
+  }
+`
+
+export default BallotRow
