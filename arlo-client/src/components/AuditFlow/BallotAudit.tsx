bdd22d374d575a156c8e6fb8128189f4e1b9f4a1
morgan <morgan.j.love@gmail.com>
2019-09-16 16:51:57 -0700

    vote audit form

--- arlo-client/src/components/AuditFlow/BallotAudit.tsx
+++ arlo-client/src/components/AuditFlow/BallotAudit.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Formik, FormikProps, Form, getIn } from 'formik'
-import { H4, H3, Divider, RadioGroup, Radio } from '@blueprintjs/core'
+import { H4, H3, Divider, RadioGroup } from '@blueprintjs/core'
 import styled from 'styled-components'
 import BallotRow from './BallotRow'
+import BlockRadio from './BlockRadio'
+import FormButton from '../Form/FormButton'
 import { Ballot } from '../../types'
 
 const FormBlock = styled(Form)`
   background-color: #ced9e0;
+  padding: 20px;
+`
+
+const RadioGroupFlex = styled(RadioGroup)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding-top: 20px;
+`
+
+const ProgressActions = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  margin-top: 20px;
 `
 
 interface Props {
@@ -40,25 +56,50 @@ const BallotAudit: React.FC<Props> = ({ review, vote, setVote }: Props) => {
             review()
           }}
           render={({
-            handleChange,
+            handleSubmit,
             values,
             setFieldValue,
           }: FormikProps<Options>) => {
+            const radioProps = {
+              name: 'vote',
+              handleChange: (e: React.ChangeEvent<HTMLInputElement>) =>
+                setFieldValue('vote', e.currentTarget.value),
+            }
             return (
-              <FormBlock>
-                <H3>[insert name of choice here]</H3>
-                <Divider />
-                <RadioGroup
-                  name="vote"
-                  onChange={e => setFieldValue('vote', e.currentTarget.value)}
-                  selectedValue={getIn(values, 'vote')}
-                >
-                  <Radio value="YES">Yes/For</Radio>
-                  <Radio value="NO">No/Against</Radio>
-                  <Radio value="NO_CONSENSUS">No audit board consensus</Radio>
-                  <Radio value="NO_VOTE">Blank vote/no mark</Radio>
-                </RadioGroup>
-              </FormBlock>
+              <>
+                <FormBlock>
+                  <H3>[insert name of choice here]</H3>
+                  <Divider />
+                  <RadioGroupFlex
+                    name="vote"
+                    onChange={() => null} // required by blueprintjs but we're implementing on BlockRadio instead
+                    selectedValue={getIn(values, 'vote')}
+                  >
+                    <BlockRadio {...radioProps} value="YES">
+                      Yes/For
+                    </BlockRadio>
+                    <BlockRadio {...radioProps} value="NO">
+                      No/Against
+                    </BlockRadio>
+                    <BlockRadio {...radioProps} value="NO_CONSENSUS">
+                      No audit board consensus
+                    </BlockRadio>
+                    <BlockRadio {...radioProps} value="NO_VOTE">
+                      Blank vote/no mark
+                    </BlockRadio>
+                  </RadioGroupFlex>
+                </FormBlock>
+                <ProgressActions>
+                  <FormButton
+                    type="submit"
+                    onClick={handleSubmit}
+                    disabled={!values.vote}
+                    intent="success"
+                  >
+                    Review
+                  </FormButton>
+                </ProgressActions>
+              </>
             )
           }}
         />
