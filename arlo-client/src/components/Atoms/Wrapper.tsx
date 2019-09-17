5064164967af199ff50e121291c2f2e7c8bce4f8
morgan <morgan.j.love@gmail.com>
2019-09-17 12:14:26 -0700

    refactoring atomic components; making styling consistent

---
+++ arlo-client/src/components/Atoms/Wrapper.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  flex-grow: 1;
+  margin-top: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  width: 100%;
+  max-width: 1020px;
+  padding-right: 15px;
+  padding-left: 15px;
+`
+
+export default Wrapper
--- arlo-client/src/components/AuditForms/index.tsx
+++ arlo-client/src/components/AuditForms/index.tsx
@@ -1,22 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import styled from 'styled-components'
 import EstimateSampleSize from './EstimateSampleSize'
 import SelectBallotsToAudit from './SelectBallotsToAudit'
 import CalculateRiskMeasurement from './CalculateRiskMeasurement'
 import { api } from '../utilities'
 import { Audit, CreateAuditParams } from '../../types'
 import ResetButton from './ResetButton'
-
-const Wrapper = styled.div`
-  flex-grow: 1;
-  margin-top: 20px;
-  margin-right: auto;
-  margin-left: auto;
-  width: 100%;
-  max-width: 1020px;
-  padding-right: 15px;
-  padding-left: 15px;
-`
+import Wrapper from '../Atoms/Wrapper'
 
 const initialData: Audit = {
   name: '',
