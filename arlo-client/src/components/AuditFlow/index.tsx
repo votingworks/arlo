a1828bcfa22bf04ee8f8b8ada5da2ebf664a3733
morgan <morgan.j.love@gmail.com>
2019-09-20 12:32:30 -0700

    initial unit tests for audit flow

--- arlo-client/src/components/AuditFlow/index.tsx
+++ arlo-client/src/components/AuditFlow/index.tsx
@@ -5,61 +5,11 @@ import { History } from 'history'
 import { IAuditFlowParams, IAudit, IAuditBoard } from '../../types'
 import { api } from '../utilities'
 import { statusStates } from '../AuditForms/_mocks'
+import { dummyBoard } from './_mocks'
 import BoardTable from './BoardTable'
 import MemberForm from './MemberForm'
 import Ballot from './Ballot'
 import Wrapper from '../Atoms/Wrapper'
-
-const rand = (max: number = 100, min: number = 1) =>
-  Math.floor(Math.random() * (+max - +min)) + +min
-
-const dummyBoard: IAuditBoard[] = [
-  {
-    id: '123',
-    name: 'Audit Board #1',
-    members: [],
-  },
-  {
-    id: '123',
-    name: 'Audit Board #1',
-    members: [
-      {
-        name: 'John Doe',
-        affiliation: '',
-      },
-      {
-        name: 'Jane Doe',
-        affiliation: 'LIB',
-      },
-    ],
-  },
-  {
-    id: '123',
-    name: 'Audit Board #1',
-    members: [
-      {
-        name: 'John Doe',
-        affiliation: '',
-      },
-      {
-        name: 'Jane Doe',
-        affiliation: 'LIB',
-      },
-    ],
-    ballots: Array(10)
-      .fill('')
-      .map(() => ({
-        tabulator: '' + rand(),
-        batch: `Precinct ${rand()}`,
-        position: '' + rand(2000),
-        status: ['AUDITED', 'NOT_AUDITED'][rand(2, 0)] as
-          | 'AUDITED'
-          | 'NOT_AUDITED',
-        vote: null,
-        comment: '',
-      })),
-  },
-]
 
 interface IProps {
   match: {
@@ -162,8 +112,6 @@ const AuditFlow: React.FC<IProps> = ({
     return (
       <Wrapper>
         <MemberForm
-          isLoading={isLoading}
-          setIsLoading={setIsLoading}
           setDummy={setDummy}
           boardName={board.name}
           jurisdictionName={audit.jurisdictions[0].name}
