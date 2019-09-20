7f77a0ffd0a4beaea94cb671b423551b2d129d63
morgan <morgan.j.love@gmail.com>
2019-09-20 13:36:40 -0700

    100% coverage on MemberForm

--- arlo-client/src/components/AuditFlow/MemberForm.test.tsx
+++ arlo-client/src/components/AuditFlow/MemberForm.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, wait } from '@testing-library/react'
 import MemberForm from './MemberForm'
 
 describe('MemberForm', () => {
@@ -12,5 +12,36 @@ describe('MemberForm', () => {
       />
     )
     expect(container).toMatchSnapshot()
+  })
+
+  it('handles inputs', async () => {
+    const setDummyMock = jest.fn()
+    const { queryAllByLabelText, queryAllByText, getByText } = render(
+      <MemberForm
+        setDummy={setDummyMock}
+        boardName="board name"
+        jurisdictionName="jurisdiction name"
+      />
+    )
+
+    const names = queryAllByLabelText('Full Name') as HTMLInputElement[]
+    const parties = queryAllByText('Democrat') as HTMLInputElement[]
+    expect(names.length).toBe(2)
+    expect(parties.length).toBe(2)
+    names &&
+      names.forEach(nameInput => {
+        fireEvent.change(nameInput, { target: { value: 'my name' } })
+        expect(nameInput.value)
+      })
+    parties &&
+      parties.forEach(partyInput => {
+        fireEvent.click(partyInput, { bubbles: true })
+      })
+
+    const nextButton = getByText('Next')
+    fireEvent.click(nextButton, { bubbles: true })
+    await wait(() => {
+      expect(setDummyMock).toBeCalled()
+    })
   })
 })
