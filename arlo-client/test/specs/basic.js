d40871caccc17a2940026351c4a1cef45306f576
morgan <morgan.j.love@gmail.com>
2019-10-14 14:00:37 -0500

    adding basic specs

---
+++ arlo-client/test/specs/basic.js
@@ -0,0 +1,12 @@
+const assert = require('assert')
+
+describe('webdriver.io page', () => {
+  it('should have the right title', () => {
+    browser.url('https://webdriver.io')
+    const title = browser.getTitle()
+    assert.strictEqual(
+      title,
+      'WebdriverIO · Next-gen WebDriver test framework for Node.js'
+    )
+  })
+})
