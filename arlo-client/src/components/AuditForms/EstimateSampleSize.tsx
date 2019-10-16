ffaf2964da3f750e057b83f1010e7c0448eec993
Morgan Love <morgan.j.love@gmail.com>
2019-10-16 14:20:12 -0700

    fixing default value for new candidate/choice

--- arlo-client/src/components/AuditForms/EstimateSampleSize.tsx
+++ arlo-client/src/components/AuditForms/EstimateSampleSize.tsx
@@ -344,7 +344,7 @@ const EstimateSampleSize: React.FC<IProps> = ({
                                       onClick={() =>
                                         choicesArrayHelpers.push({
                                           name: '',
-                                          numVotes: 0,
+                                          numVotes: '',
                                         })
                                       }
                                     >
