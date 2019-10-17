votingworks/arlo

commit ec496e7b3780c4af66909f32d8d9eeb280067d6b

Changed variable name at @benadida's suggestion

app.py
@@ -53,7 +53,7 @@ def contest_status(election):
             [choice.id, choice.num_votes]
             for choice in contest.choices])
         contests[contest.id]['ballots'] = contest.total_ballots_cast
-        contests[contest.id]['winners'] = contest.winners
+        contests[contest.id]['numWinners'] = contest.num_winners
 
     return contests
 
@@ -273,7 +273,7 @@ def audit_status(election_id = None):
                     }
                     for choice in contest.choices],
                 "totalBallotsCast": contest.total_ballots_cast,
-                "winners": contest.winners
+                "numWinners": contest.num_winners
             }
             for contest in election.contests],
         jurisdictions=[
@@ -336,7 +336,7 @@ def audit_basic_update(election_id=None):
                              id = contest['id'],
                              name = contest['name'],
                              total_ballots_cast = contest['totalBallotsCast'],
-                             winners = contest['winners'])
+                             num_winners = contest['numWinners'])
         db.session.add(contest_obj)
 
         for choice in contest['choices']:
@@ -524,7 +524,7 @@ def audit_report(election_id=None):
     choices = contest.choices
     
     report_writer.writerow(["Contest Name", contest.name])
-    report_writer.writerow(["Winners", contest.winners])
+    report_writer.writerow(["Number of Winners", contest.num_winners])
     report_writer.writerow(["Total Ballots Cast", contest.total_ballots_cast])
 
     for choice in choices:

tests/test_app.py
@@ -95,7 +95,7 @@ def setup_whole_audit(client, election_id, name, risk_limit, random_seed):
                     ],
 
                     "totalBallotsCast": 86147,
-                    "winners": 1
+                    "numWinners": 1
                 }
             ]
         })
@@ -287,7 +287,7 @@ def test_small_election(client):
                     ],
 
                     "totalBallotsCast": 2123,
-                    "winners": 1,
+                    "numWinners": 1,
                 }
             ]
         })

models.py
@@ -65,7 +65,7 @@ class TargetedContest(db.Model):
     election_id = db.Column(db.String(200), db.ForeignKey('election.id', ondelete='cascade'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     total_ballots_cast = db.Column(db.Integer, nullable=False)
-    winners = db.Column(db.Integer, nullable=False)
+    num_winners = db.Column(db.Integer, nullable=False)
 
     choices = relationship('TargetedContestChoice', backref='contest', passive_deletes=True)
     results = relationship('RoundContestResult', backref='contest', passive_deletes=True)

sampler.py
@@ -89,14 +89,14 @@ class Sampler:
             cand_vec = sorted(
                     [(cand, self.contests[contest][cand]) 
                         for cand in self.contests[contest]
-                            if cand not in ['winners', 'ballots']
+                            if cand not in ['numWinners', 'ballots']
                     ], 
             key=operator.itemgetter(1), reverse = True)
 
-            if 'winners' not in self.contests[contest]:
+            if 'numWinners' not in self.contests[contest]:
                 num_winners = 1
             else:
-                num_winners = self.contests[contest]['winners']
+                num_winners = self.contests[contest]['numWinners']
             winners = cand_vec[:num_winners]
             losers = cand_vec[num_winners:]
 
@@ -378,7 +378,7 @@ class Sampler:
 
 
             # For multi-winner, do nothing
-            if 'winners' not in self.contests[contest] or self.contests[contest]['winners'] != 1:
+            if 'numWinners' not in self.contests[contest] or self.contests[contest]['numWinners'] != 1:
                 samples[contest] = {
                     'asn': {
                         'size': asns[contest],

tests/test_sampler.py
@@ -14,51 +14,51 @@ def sampler():
             'cand1': 600,
             'cand2': 400,
             'ballots': 1000,
-            'winners': 1
+            'numWinners': 1
         },
         'test2': {
             'cand1': 600,
             'cand2': 200,
             'cand3': 100,
             'ballots': 900,
-            'winners': 1
+            'numWinners': 1
         },
         'test3': {
             'cand1': 100,
             'ballots': 100,
-            'winners': 1
+            'numWinners': 1
         },
         'test4': {
             'cand1': 100,
             'ballots': 100,
-            'winners': 1
+            'numWinners': 1
         },
         'test5': {
             'cand1' : 500,
             'cand2': 500,
             'ballots': 1000,
-            'winners': 1
+            'numWinners': 1
         },
         'test6': {
             'cand1': 300,
             'cand2': 200,
             'cand3': 200,
             'ballots': 1000,
-            'winners': 1
+            'numWinners': 1
         },
         'test7': {
             'cand1': 300,
             'cand2': 200,
             'cand3': 100,
             'ballots': 700,
-            'winners': 2
+            'numWinners': 2
         },
         'test7': {
             'cand1': 300,
             'cand2': 200,
             'cand3': 100,
             'ballots': 700,
-            'winners': 2
+            'numWinners': 2
 
         },
         'test8': {
@@ -66,21 +66,21 @@ def sampler():
             'cand2': 300,
             'cand3': 100,
             'ballots': 700,
-            'winners': 2
+            'numWinners': 2
 
         },
         'test9': {
             'cand1': 300,
             'cand2': 200,
             'ballots': 700,
-            'winners': 2
+            'numWinners': 2
         },
         'test10': {
             'cand1': 600,
             'cand2': 300,
             'cand3': 100,
             'ballots': 1000,
-            'winners': 2
+            'numWinners': 2
         },
     }
 
@@ -445,7 +445,7 @@ def test_get_sample_sizes(sampler):
         for key in true_sample_sizes[contest]:
             if key == 'asn': 
                 # Check probs:
-                if sampler.contests[contest]['winners'] == 1:
+                if sampler.contests[contest]['numWinners'] == 1:
                     expected_prob = true_sample_sizes[contest][key]['prob']
                     computed_prob = round(computed_samples[contest][key]['prob'], 2)
 
@@ -457,7 +457,7 @@ def test_get_sample_sizes(sampler):
                 expected =  true_sample_sizes[contest][key]
                 computed = computed_samples[contest][key]
             diff = expected - computed
-            # TODO are these tolerances acceptable?
+
             assert not diff , '{} sample size for {} failed: got {}, expected {}'.format(key, contest, computed, expected)
 
 def test_draw_sample(sampler):

commit 63b86fbb6c61fc7eed852a579571b368d08a5989

Fixing bug introduced by #134

app.py
@@ -336,7 +336,7 @@ def audit_basic_update(election_id=None):
                              id = contest['id'],
                              name = contest['name'],
                              total_ballots_cast = contest['totalBallotsCast'],
-                             num_winners = contest['numWinners'])
+                             num_winners = contest['winners'])
         db.session.add(contest_obj)
 
         for choice in contest['choices']:

tests/test_app.py
@@ -95,7 +95,7 @@ def setup_whole_audit(client, election_id, name, risk_limit, random_seed):
                     ],
 
                     "totalBallotsCast": 86147,
-                    "numWinners": 1
+                    "winners": 1
                 }
             ]
         })
@@ -287,7 +287,7 @@ def test_small_election(client):
                     ],
 
                     "totalBallotsCast": 2123,
-                    "numWinners": 1,
+                    "winners": 1,
                 }
             ]
         })