6bfb7104cee7a516d7a57c96ebce4507ec3fb620
Morgan Love <morgan.j.love@gmail.com>
2019-10-24 11:55:01 -0700

    cleanup

--- arlo-client/wdio.conf.js
+++ arlo-client/wdio.conf.js
@@ -177,9 +177,8 @@ exports.config = {
   // before: function (capabilities, specs) {
   // },
   before: function() {
-    require('ts-node').register({ files: true })
     require('@babel/register')({
-      extensions: ['.ts', '.es6', '.es', '.jsx', '.js', '.mjs'],
+      extensions: ['.ts'],
       presets: [
         [
           '@babel/preset-env',
