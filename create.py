cc1207656d4ecd422fa672e3151666c73d369758
Ben Adida <ben@adida.net>
2019-11-25 11:38:02 -0800

    catch on database existence check

--- create.py
+++ create.py
@@ -7,9 +7,13 @@ if __name__ == '__main__':
     engine = create_engine(DATABASE_URL)
     print(f'database: {engine.url}')
 
-    if not database_exists(engine.url):
-        print('creating database…')
-        create_database(engine.url)
+    try:
+        if not database_exists(engine.url):
+            print('creating database…')
+            create_database(engine.url)
+    except:
+        # sometimes, e.g. on Heroku, you can't even test if the database exists cause permissions
+        pass
 
     print('creating tables…')
     init_db()
