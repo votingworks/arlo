f959e10e5982c5603b2ef13dfff233a25a6028b5
Ben Adida <ben@adida.net>
2019-11-25 11:38:02 -0800

    postgres fix to tolerate error at checking db existence

--- resetdb.py
+++ resetdb.py
@@ -7,9 +7,13 @@ if __name__ == '__main__':
     engine = create_engine(DATABASE_URL)
     print(f'database: {engine.url}')
 
-    if database_exists(engine.url):
-        print('dropping database…')
-        drop_database(engine.url)
+    try:
+        if database_exists(engine.url):
+            print('dropping database…')
+            drop_database(engine.url)
+    except:
+        # sometimes, e.g. on Heroku, we don't have permission to check the DB exists.
+        print('unable to check that DB exists, assuming it does...')
 
     print('creating database…')
     create_database(engine.url)
