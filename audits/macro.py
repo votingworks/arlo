a412abe7ae5df6d04b2f80ab569fb1d2bca8b94a
Matt Bernhard <mdb92nc@gmail.com>
2019-12-09 13:52:52 -0500

    Update audits/macro.py
    Co-Authored-By: Brian Donovan <1938+eventualbuddha@users.noreply.github.com>

--- audits/macro.py
+++ audits/macro.py
@@ -117,7 +117,7 @@ class MACRO(RiskLimitingAudit):
         """
         Computes initial sample sizes parameterized by likelihood that the
         initial sample will confirm the election result, assuming no
-        discrpancies.
+        discrepancies.
 
         Inputs:
             sample_results - if a sample has already been drawn, this will
