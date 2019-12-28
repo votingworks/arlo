bad710b00d409604720ae4cef3ee942839443ae7
Morgan Love <morgan.j.love@gmail.com>
2019-12-27 17:32:08 -0800

    improving wdio reporters

--- arlo-client/wdio.conf.js
+++ arlo-client/wdio.conf.js
@@ -127,7 +127,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter.html
-  reporters: ['dot'],
+  reporters: ['dot', 'spec'],
 
   //
   // Options to be passed to Jasmine.
