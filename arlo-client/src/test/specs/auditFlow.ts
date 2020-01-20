5503917fd63a3daacbeb52c2765bfd1ec5817a53
Ben Adida <ben@adida.net>
2020-01-19 20:28:08 -0800

    fix webdriver tests, add some robustness to them too

--- arlo-client/src/test/specs/auditFlow.ts
+++ arlo-client/src/test/specs/auditFlow.ts
@@ -8,13 +8,17 @@ import {
 // import { voteValue } from '../../components/AuditFlow/BlockRadio'
 
 const ballotNext = (option: string) => {
-  const callout = $('.bp3-callout*=auditing ballot')
-    .getText()
-    .split(' ')
+  const calloutElement = $('.bp3-callout*=auditing ballot')
+  calloutElement.waitForExist()
+  const callout = calloutElement.getText().split(' ')
   const ballot = Number(callout[callout.length - 3])
   const lastBallot = Number(callout[callout.length - 1])
-  $(`.radio-text=${option}`).click()
-  $('.bp3-button-text=Review').click()
+  const radioButton = $(`.radio-text=${option}`)
+  radioButton.waitForExist()
+  radioButton.click()
+  const reviewButton = $('.bp3-button-text=Review')
+  reviewButton.waitForExist()
+  reviewButton.click()
   const submit = $('.bp3-button-text=Submit & Next Ballot')
   submit.waitForExist()
   submit.click()
@@ -64,12 +68,10 @@ beforeEach(() => {
 describe('audit flow', () => {
   it('has a happy path', () => {
     $('a=Start Auditing').click()
-    ballotNext('Choice One')
-    ballotNext('Yes/For')
-    for (let i = 0; ballotNext('Yes/For') && i < 20; i++) {
-      $('.bp3-callout*=auditing ballot').waitForExist()
+    for (let i = 0; i < 20; i++) {
+      ballotNext('Choice One')
     }
-    if (ballotNext('Yes/For')) {
+    if (ballotNext('Choice One')) {
       $('.bp3-button=Return to audit overview').click()
     }
     $('h1*=Ballot Cards to Audit').waitForExist(10000)
@@ -77,11 +79,11 @@ describe('audit flow', () => {
 
   it('handles all four voting options', () => {
     $('a=Start Auditing').click()
-    ballotNext('Yes/For')
+    ballotNext('Choice One')
     $('.bp3-callout*=auditing ballot 2 of').waitForExist()
-    ballotNext('No/Against')
+    ballotNext('Choice Two')
     $('.bp3-callout*=auditing ballot 3 of').waitForExist()
-    ballotNext('No audit board consensus')
+    ballotNext("Audit board can't agree")
     $('.bp3-callout*=auditing ballot 4 of').waitForExist()
     ballotNext('Blank vote/no mark')
     $('.bp3-callout*=auditing ballot 5 of').waitForExist()
