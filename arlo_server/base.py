4a9e930c0d992285a0243f89ad3754a9ce05fc1b
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-03-02 16:32:49 -0800

    feat: enable sessions
    This provides a cookie-based application session with a secret key from `ARLO_SESSION_SECRET`. If not provided in development or test, a fixed one will be used instead.

--- arlo_server/base.py
+++ arlo_server/base.py
@@ -1,4 +1,5 @@
 from flask import Flask
-from config import STATIC_FOLDER
+from config import STATIC_FOLDER, SESSION_SECRET
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
+app.secret_key = SESSION_SECRET
