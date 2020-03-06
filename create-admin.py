f92ed18b932416cc7b8e69922cc1fa1834dd01fc
Ben Adida <ben@adida.net>
2020-03-06 11:48:35 -0800

    scripts to create orgs and admins

---
+++ create-admin.py
@@ -0,0 +1,17 @@
+import sys, uuid
+
+from models import *
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python create-admin.py <org_id> <user_email>")
+        sys.exit(1)
+
+    org_id, email = sys.argv[1:]
+    u = User(id=str(uuid.uuid4()), email=email, external_id=email)
+    audit_admin = AuditAdministration(user_id=u.id, organization_id=org_id)
+    db.session.add(u)
+    db.session.add(audit_admin)
+    db.session.commit()
+
+    print(u.id)
