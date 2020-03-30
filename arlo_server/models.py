0236ace73c383317cfab3fa319713472dadae14d
Jonah Kagan <jonah@voting.works>
2020-03-30 12:40:40 -0700

    Add created_at and updated_at timestamps to all db models

--- arlo_server/models.py
+++ arlo_server/models.py
@@ -1,10 +1,19 @@
 from sqlalchemy.orm import relationship, backref
+import sqlalchemy as sa
 from enum import Enum
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Model
 from flask_sqlalchemy.model import DefaultMeta
+from datetime import datetime as dt
 
 
-db = SQLAlchemy()
+class Base(Model):
+    created_at = sa.Column(sa.DateTime, default=dt.utcnow, nullable=False)
+    updated_at = sa.Column(
+        sa.DateTime, default=dt.utcnow, onupdate=dt.utcnow, nullable=False
+    )
+
+
+db = SQLAlchemy(model_class=Base)
 
 # Typing workaround from https://github.com/dropbox/sqlalchemy-stubs/issues/76#issuecomment-595839159
 BaseModel: DefaultMeta = db.Model
