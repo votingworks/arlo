482702c57da85eef12e77535ff557f9bb1f3498d
Morgan Love <morgan.j.love@gmail.com>
2020-04-03 18:13:37 -0700

    Reducing wdio maxInstances

--- arlo-client/wdio.conf.js
+++ arlo-client/wdio.conf.js
@@ -45,7 +45,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -56,7 +56,7 @@ exports.config = {
       // maxInstances can get overwritten per capability. So if you have an in-house Selenium
       // grid with only 5 firefox instances available you can make sure that not more than
       // 5 instances get started at a time.
-      maxInstances: 5,
+      maxInstances: 1,
       //
       browserName: 'chrome',
       // If outputDir is provided WebdriverIO can capture driver session logs
