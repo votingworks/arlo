ccfd534babc62d592e11d863be57e803233a42b6
Morgan Love <morgan.j.love@gmail.com>
2020-04-03 18:14:57 -0700

    increase timeout from 10000 to 20000

--- arlo-client/wdio.conf.js
+++ arlo-client/wdio.conf.js
@@ -99,7 +99,7 @@ exports.config = {
   baseUrl: 'http://localhost:3000',
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 10000,
+  waitforTimeout: 20000,
   //
   // Default timeout in milliseconds for request
   // if Selenium Grid doesn't send response
