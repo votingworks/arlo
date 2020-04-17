e0b86f47418f5d9940f647d1c08c7e5c25fb91f6
Morgan Love <morgan.j.love@gmail.com>
2020-04-16 17:32:26 -0700

    initial styling test

--- arlo-client/src/components/Audit/Setup/Review/index.tsx
+++ arlo-client/src/components/Audit/Setup/Review/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { H4, Callout } from '@blueprintjs/core'
 import FormButtonBar from '../../../Atoms/Form/FormButtonBar'
 import FormButton from '../../../Atoms/Form/FormButton'
 import { ISidebarMenuItem } from '../../../Atoms/Sidebar'
+import H2Title from '../../../Atoms/H2Title'
 
 interface IProps {
   locked: boolean
@@ -11,7 +13,14 @@ interface IProps {
 const Review: React.FC<IProps> = ({ prevStage }: IProps) => {
   return (
     <div>
-      <p>Review</p>
+      <H2Title>Review &amp; Launch</H2Title>
+      <Callout intent="warning">
+        Once the audit is started, the audit definition will no longer be
+        editable. Please make sure this data is correct before launching the
+        audit.
+      </Callout>
+      <H4>Audit Settings</H4>
+      <H4>Sample Size Options</H4>
       <FormButtonBar>
         <FormButton onClick={prevStage.activate}>Back</FormButton>
       </FormButtonBar>
