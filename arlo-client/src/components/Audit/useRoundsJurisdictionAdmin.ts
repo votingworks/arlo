c27530f4b22f83701d5f92f10fabc6f48912bc2e
Jonah Kagan <jonah@voting.works>
2020-04-30 18:40:03 -0700

    Factor out audit components by audit type and user type

---
+++ arlo-client/src/components/Audit/useRoundsJurisdictionAdmin.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { api } from '../utilities'
+
+interface IRound {
+  id: string
+  roundNum: number
+  startedAt: string
+  endedAt: string
+  isAuditComplete: boolean
+}
+
+const useRoundsJurisdictionAdmin = (
+  electionId: string,
+  jurisdictionId: string
+) => {
+  const [rounds, setRounds] = useState<IRound[] | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response: { rounds: IRound[] } = await api(
+          `/election/${electionId}/jurisdiction/${jurisdictionId}/round`
+        )
+        setRounds(response.rounds)
+      } catch (err) {
+        toast.error(err.message)
+      }
+    })()
+  }, [electionId, jurisdictionId])
+
+  return rounds
+}
+
+export default useRoundsJurisdictionAdmin
