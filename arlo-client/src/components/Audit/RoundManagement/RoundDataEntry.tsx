017da4d2d5140e806623b44938966b0f10124bdb
MorganLove <morgan.j.love@gmail.com>
2020-05-01 19:18:27 -0700

    Round management (#467)
    * restructuring components
    
    * adding create audit boards functionality
    
    * minor tweak

---
+++ arlo-client/src/components/Audit/RoundManagement/RoundDataEntry.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import H2Title from '../../Atoms/H2Title'
+
+const RoundDataEntry = () => {
+  return <H2Title>Round 1 Data Entry</H2Title>
+}
+
+export default RoundDataEntry
