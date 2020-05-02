d21e58af99bcb670464ca6a1929dfaf42a53b532
MorganLove <morgan.j.love@gmail.com>
2020-05-01 19:18:27 -0700

    Round management (#467)
    * restructuring components
    
    * adding create audit boards functionality
    
    * minor tweak

---
+++ arlo-client/src/components/Audit/RoundManagement/RoundProgress.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import H2Title from '../../Atoms/H2Title'
+
+const RoundProgress = () => {
+  return <H2Title>Round 1 Progress</H2Title>
+}
+
+export default RoundProgress
