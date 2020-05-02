ba2ab6c132cb8d5beda9099a00141d8a9f48fc82
Jonah Kagan <jonah@voting.works>
2020-05-02 15:28:19 -0700

    initial implementation

---
+++ arlo-client/src/components/Audit/useRoundsAuditAdmin.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { api } from '../utilities'
+
+export interface IRound {
+  id: string
+  roundNum: number
+  startedAt: string
+  endedAt: string
+  isAuditComplete: boolean
+}
+
+const useRoundsAuditAdmin = (electionId: string) => {
+  const [rounds, setRounds] = useState<IRound[] | null>(null)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response: { rounds: IRound[] } = await api(
+          `/election/${electionId}/round`
+        )
+        setRounds(response.rounds)
+      } catch (err) {
+        toast.error(err.message)
+      }
+    })()
+  }, [electionId])
+
+  return rounds
+}
+
+export default useRoundsAuditAdmin
