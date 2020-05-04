ebbccbf5a693f193debd036d8379251845a40982
MorganLove <morgan.j.love@gmail.com>
2020-05-04 14:51:45 -0700

    Adding progress bars (#470)
    * adding progress bars
    
    * rebasing on ja-status-bar and updating to take advantage of it

--- arlo-client/src/components/Audit/RoundManagement/RoundProgress.tsx
+++ arlo-client/src/components/Audit/RoundManagement/RoundProgress.tsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import { ProgressBar } from '@blueprintjs/core'
+import styled from 'styled-components'
 import H2Title from '../../Atoms/H2Title'
+import { IRound } from '../useRoundsJurisdictionAdmin'
+import { IAuditBoard } from '../useAuditBoards'
 
-const RoundProgress = () => {
-  return <H2Title>Round 1 Progress</H2Title>
+const MainBarWrapper = styled.div`
+  margin-bottom: 20px;
+  width: 500px;
+  font-size: 16px;
+`
+
+const SmallBarWrapper = styled.div`
+  margin-bottom: 10px;
+  width: 400px;
+`
+
+const RoundProgress = ({
+  auditBoards,
+  round,
+}: {
+  auditBoards: IAuditBoard[]
+  round: IRound
+}) => {
+  if (!auditBoards) return null
+  const sum = (ns: number[]) => ns.reduce((a, b) => a + b)
+  const auditedBallots = sum(
+    auditBoards.map(ab => ab.currentRoundStatus.numAuditedBallots)
+  )
+  const sampledBallots = sum(
+    auditBoards.map(ab => ab.currentRoundStatus.numSampledBallots)
+  )
+  return (
+    <>
+      <H2Title>Round {round.roundNum} Progress</H2Title>
+      <MainBarWrapper>
+        <span>
+          {auditedBallots} of {sampledBallots} ballots audited{' '}
+        </span>
+        <ProgressBar
+          value={auditedBallots / sampledBallots}
+          animate={auditedBallots < sampledBallots}
+          intent="primary"
+        />
+      </MainBarWrapper>
+      {auditBoards.map(
+        ({
+          id,
+          name,
+          currentRoundStatus: { numAuditedBallots, numSampledBallots },
+        }) => (
+          <SmallBarWrapper key={id}>
+            <span>{`${name}: ${numAuditedBallots} of ${numSampledBallots} ballots audited `}</span>
+            <ProgressBar
+              value={numAuditedBallots / numSampledBallots}
+              animate={numAuditedBallots < numSampledBallots}
+              intent="primary"
+            />
+          </SmallBarWrapper>
+        )
+      )}
+    </>
+  )
 }
 
 export default RoundProgress
