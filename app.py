124574a613549689204fb69a1a34a8d8d923c439
Ben Adida <ben@adida.net>
2020-05-06 21:40:00 -0700

    use reloader only in dev environments

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 from arlo_server import app
 
+from config import FLASK_ENV, DEVELOPMENT_ENVS
+
 if __name__ == "__main__":
     app.run(
-        use_reloader=True,
+        use_reloader=FLASK_ENV in DEVELOPMENT_ENVS,
         port=os.environ.get("PORT", 3001),
         host="0.0.0.0",
         threaded=True,
