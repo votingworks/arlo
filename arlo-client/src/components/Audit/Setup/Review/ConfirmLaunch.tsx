5bee5bb5d8bec20f019efc5c7ed0cdd9833a8770
MorganLove <morgan.j.love@gmail.com>
2020-05-08 14:52:40 -0700

    adding x of y jurisdictions have uploaded ballot manifests note to confirmation dialog (#520)

--- arlo-client/src/components/Audit/Setup/Review/ConfirmLaunch.tsx
+++ arlo-client/src/components/Audit/Setup/Review/ConfirmLaunch.tsx
@@ -15,10 +15,14 @@ const ConfirmLaunch = ({
   isOpen,
   handleClose,
   onLaunch,
+  numJurisdictions,
+  completedBallotUploads,
 }: {
   isOpen: boolean
   handleClose: () => void
   onLaunch: () => void
+  numJurisdictions: number
+  completedBallotUploads: number
 }) => {
   return (
     <Dialog
@@ -29,6 +33,10 @@ const ConfirmLaunch = ({
     >
       <div className={Classes.DIALOG_BODY}>
         <p>This action cannot be undone</p>
+        <p>
+          {completedBallotUploads} of {numJurisdictions} jurisdictions have
+          uploaded ballot manifests.
+        </p>
       </div>
       <div className={Classes.DIALOG_FOOTER}>
         <div className={Classes.DIALOG_FOOTER_ACTIONS}>
