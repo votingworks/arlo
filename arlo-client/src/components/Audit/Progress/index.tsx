82336677687201d4ab36b5192b0ffca1041f6d5e
MorganLove <morgan.j.love@gmail.com>
2020-05-08 20:13:08 -0700

    Jurisdiction detail dialog (#523)
    * Adding dialog with jurisdiction details
    
    * tweaking status reporting

--- arlo-client/src/components/Audit/Progress/index.tsx
+++ arlo-client/src/components/Audit/Progress/index.tsx
@@ -3,7 +3,13 @@ import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import { Table, Column, Cell } from '@blueprintjs/table'
 import H2Title from '../../Atoms/H2Title'
-import useJurisdictions, { JurisdictionRoundStatus } from '../useJurisdictions'
+import useJurisdictions, {
+  JurisdictionRoundStatus,
+  IJurisdiction,
+  prettifyStatus,
+} from '../useJurisdictions'
+import FormButton from '../../Atoms/Form/FormButton'
+import JurisdictionDetail from './JurisdictionDetail'
 
 const Wrapper = styled.div`
   flex-grow: 1;
@@ -20,13 +26,29 @@ interface IProps {
 const Progress: React.FC<IProps> = ({ refreshId }: IProps) => {
   const { electionId } = useParams<{ electionId: string }>()
   const jurisdictions = useJurisdictions(electionId, refreshId)
+  const [
+    jurisdictionDetail,
+    setJurisdictionDetail,
+  ] = useState<IJurisdiction | null>(null)
+  const openDetail = (e: React.FormEvent, index: number) => {
+    setJurisdictionDetail(jurisdictions[index])
+  }
 
   const columns = [
     <Column
       key="name"
       name="Jurisdiction Name"
       cellRenderer={(row: number) => (
-        <PaddedCell>{jurisdictions[row].name}</PaddedCell>
+        <PaddedCell>
+          <FormButton
+            size="sm"
+            intent="primary"
+            minimal
+            onClick={e => openDetail(e, row)}
+          >
+            {jurisdictions[row].name}
+          </FormButton>
+        </PaddedCell>
       )}
     />,
     <Column
@@ -36,24 +58,16 @@ const Progress: React.FC<IProps> = ({ refreshId }: IProps) => {
         const { ballotManifest, currentRoundStatus } = jurisdictions[row]
         if (!currentRoundStatus) {
           const { processing } = ballotManifest
-          switch (processing && processing.status) {
-            case 'ERRORED':
-              return <PaddedCell>Manifest upload failed</PaddedCell>
-            case 'PROCESSED':
-              return <PaddedCell>Manifest received</PaddedCell>
-            default:
-              return <PaddedCell>No manifest uploaded</PaddedCell>
-          }
-        } else {
-          const prettyStatus = {
-            [JurisdictionRoundStatus.NOT_STARTED]: 'Not started',
-            [JurisdictionRoundStatus.IN_PROGRESS]: 'In progress',
-            [JurisdictionRoundStatus.COMPLETE]: 'Complete',
-          }
-          return (
-            <PaddedCell>{prettyStatus[currentRoundStatus.status]}</PaddedCell>
-          )
+          return <PaddedCell>{prettifyStatus(processing)}</PaddedCell>
         }
+        const prettyStatus = {
+          [JurisdictionRoundStatus.NOT_STARTED]: 'Not started',
+          [JurisdictionRoundStatus.IN_PROGRESS]: 'In progress',
+          [JurisdictionRoundStatus.COMPLETE]: 'Complete',
+        }
+        return (
+          <PaddedCell>{prettyStatus[currentRoundStatus.status]}</PaddedCell>
+        )
       }}
     />,
     <Column
@@ -109,6 +123,11 @@ const Progress: React.FC<IProps> = ({ refreshId }: IProps) => {
           {columns}
         </Table>
       </div>
+      <JurisdictionDetail
+        jurisdiction={jurisdictionDetail}
+        electionId={electionId}
+        handleClose={() => setJurisdictionDetail(null)}
+      />
     </Wrapper>
   )
 }
