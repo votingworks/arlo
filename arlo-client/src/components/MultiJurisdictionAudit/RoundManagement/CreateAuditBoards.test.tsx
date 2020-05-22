de23a8a097777f3530c4bff79223a9d95bca5139
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-05-22 08:00:22 -0700

    fix: ensure audit board names are sorted correctly
    This won't fix any existing audit board names, but for new ones it will ensure that sorting lexicographically also sorts numerically. This was the simplest fix I could think of since the client controls the names, but the server does the sorting.

---
+++ arlo-client/src/components/MultiJurisdictionAudit/RoundManagement/CreateAuditBoards.test.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { render, wait, fireEvent } from '@testing-library/react'
+import CreateAuditBoards from './CreateAuditBoards'
+
+test('names audit boards numerically', async () => {
+  const createAuditBoards = jest.fn()
+  const { getByText, getByTestId } = render(
+    <CreateAuditBoards
+      auditBoards={[]}
+      numBallots={10}
+      roundNum={1}
+      createAuditBoards={createAuditBoards}
+    />
+  )
+
+  fireEvent.change(getByTestId('numAuditBoards'), { target: { value: '3' } })
+  fireEvent.click(getByText('Save & Next'))
+
+  await wait(() => {
+    expect(createAuditBoards).toHaveBeenCalledWith([
+      { name: 'Audit Board #1' },
+      { name: 'Audit Board #2' },
+      { name: 'Audit Board #3' },
+    ])
+  })
+})
+
+test('names audit boards such that the names sort sensibly', async () => {
+  const createAuditBoards = jest.fn()
+  const { getByText, getByTestId } = render(
+    <CreateAuditBoards
+      auditBoards={[]}
+      numBallots={10}
+      roundNum={1}
+      createAuditBoards={createAuditBoards}
+    />
+  )
+
+  fireEvent.change(getByTestId('numAuditBoards'), { target: { value: '10' } })
+  fireEvent.click(getByText('Save & Next'))
+
+  await wait(() => {
+    expect(createAuditBoards).toHaveBeenCalledWith([
+      { name: 'Audit Board #01' },
+      { name: 'Audit Board #02' },
+      { name: 'Audit Board #03' },
+      { name: 'Audit Board #04' },
+      { name: 'Audit Board #05' },
+      { name: 'Audit Board #06' },
+      { name: 'Audit Board #07' },
+      { name: 'Audit Board #08' },
+      { name: 'Audit Board #09' },
+      { name: 'Audit Board #10' },
+    ])
+  })
+})
