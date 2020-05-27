96909567d36c8029d0fabe218c23f850018458b6
Jonah Kagan <jonah@voting.works>
2020-05-26 17:55:34 -0700

    /sample-sizes always return round one sample size options
    This endpoint is only used for the audit setup flow. Currently, if you
    navigate back to the Review & Launch screen after starting an audit, the
    sample sizes shown will reflect the current state of the audit. This fix
    ensures that they will always show the initial sample size options that
    the user chose from for round 1 when starting the audit.

--- arlo_server/sample_sizes.py
+++ arlo_server/sample_sizes.py
@@ -17,7 +17,7 @@ def cumulative_contest_results(contest: Contest) -> Dict[str, int]:
     return results_by_choice
 
 
-def sample_size_options(election: Election) -> dict:
+def sample_size_options(election: Election, round_one=False) -> dict:
     if not election.contests:
         raise BadRequest("Cannot compute sample sizes until contests are set")
     if not election.risk_limit:
@@ -26,10 +26,20 @@ def sample_size_options(election: Election) -> dict:
     # For now, we only support one targeted contest
     contest = next(c for c in election.contests if c.is_targeted)
 
+    # Because the /sample-sizes endpoint is only used for the audit setup flow,
+    # we always want it to return the sample size options for the first round.
+    # So we support a flag in this function to compute the sample sizes for
+    # round one specifically, even if the audit has progressed further.
+    cumulative_results = (
+        {choice.id: 0 for choice in contest.choices}
+        if round_one
+        else cumulative_contest_results(contest)
+    )
+
     sample_sizes: dict = bravo.get_sample_size(
         election.risk_limit / 100,
         sampler_contest.from_db_contest(contest),
-        cumulative_contest_results(contest),
+        cumulative_results,
     )
     return sample_sizes
 
@@ -37,7 +47,7 @@ def sample_size_options(election: Election) -> dict:
 @app.route("/election/<election_id>/sample-sizes", methods=["GET"])
 @with_election_access
 def get_sample_sizes(election: Election):
-    sample_sizes = sample_size_options(election)
+    sample_sizes = sample_size_options(election, round_one=True)
 
     # Convert the results into a slightly more regular format
     json_sizes = list(sample_sizes.values())
