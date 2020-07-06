d6c873c30cf47dd8baf7867afc3ef0cbd4949fba
Jonah Kagan <jonah@voting.works>
2020-07-06 11:42:09 -0700

    begin setting up alembic

--- server/database.py
+++ server/database.py
@@ -1,5 +1,5 @@
 import re
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker, Query
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from .config import DATABASE_URL
@@ -9,8 +9,18 @@ from .config import DATABASE_URL
 engine = create_engine(DATABASE_URL)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
 
+meta = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_N_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
 
-@as_declarative()
+
+@as_declarative(metadata=meta)
 class Base:
     query: Query = db_session.query_property()
     # pylint: disable=no-self-argument,no-member
