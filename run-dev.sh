949d6268e882963ceea589cdc1b13bfbbc2b5178
Jonah Kagan <jonah@voting.works>
2020-07-22 12:14:20 -0700

    one run script for cypress and wdio

--- run-dev.sh
+++ run-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export FLASK_ENV=development
+export FLASK_ENV=${FLASK_ENV:-development}
 trap 'kill 0' SIGINT SIGHUP
 pipenv run python -m server.main &
 pipenv run python -m server.bgcompute &
