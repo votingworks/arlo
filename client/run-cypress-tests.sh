da8849635be6a68aaa4b8a6e4e6516c4b75c53e2
Jonah Kagan <jonah@voting.works>
2020-07-22 16:19:48 -0700

    don't trap exit for cypress

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -3,7 +3,7 @@
 export ARLO_AUDITADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 export ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 
-trap 'kill 0' SIGINT SIGHUP EXIT
+trap 'kill 0' SIGINT SIGHUP
 ./cypress/seed-test-db.sh
 pushd ..
 FLASK_ENV=test ./run-dev.sh &
