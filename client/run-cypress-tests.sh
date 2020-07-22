d3c81873ed657c4a05b23b7f8cde6c691316f97e
Jonah Kagan <jonah@voting.works>
2020-07-22 16:18:34 -0700

    env vars

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export ARLO_AUDITADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
+export ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
+
 trap 'kill 0' SIGINT SIGHUP EXIT
 ./cypress/seed-test-db.sh
 pushd ..
