f74556d9da17f8f615799f6d22c47cbd8a7ecc92
Morgan Love <morgan.j.love@gmail.com>
2020-07-28 16:26:55 -0700

    adding initial test

--- client/cypress/end-to-end/ballot-polling-basic.spec.js
+++ client/cypress/end-to-end/ballot-polling-basic.spec.js
@@ -4,3 +4,16 @@ describe('Basic ballot polling audit', () => {
     cy.loginAuditAdmin('audit-admin-cypress@example.com')
   })
 })
+
+describe('Audit setup', () => {
+  it('Creates an audit', () => {
+    cy.visit('/')
+    cy.loginAuditAdmin('audit-admin-cypress@example.com')
+    cy.contains('Give your new audit a unique name.').type('Audit One')
+    cy.contains('Create a New Audit').click()
+    cy.url()
+      .should('include', '/setup')
+      .and('include', '/election/')
+    cy.get('h4').should('contain', 'The audit has not started.')
+  })
+})
