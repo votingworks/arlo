f49b16848069d6afada77d147c83be38fba54967
Jonah Kagan <jonah@voting.works>
2020-07-29 12:31:08 -0700

    Disallow deleting audits in production

--- server/superadmin.py
+++ server/superadmin.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, request, Blueprint
+from werkzeug.exceptions import Forbidden
 
 from .models import *  # pylint: disable=wildcard-import
 from .database import db_session
@@ -7,6 +8,7 @@ from .auth import (
     with_superadmin_access,
     set_loggedin_user,
 )
+from .config import FLASK_ENV
 
 superadmin = Blueprint("superadmin", __name__)
 
@@ -53,6 +55,8 @@ def superadmin_jurisdictionadmin_login():
 @superadmin.route("/superadmin/delete-election/<election_id>", methods=["POST"])
 @with_superadmin_access
 def superadmin_delete_election(election_id: str):
+    if FLASK_ENV == "production":  # pragma: no cover
+        raise Forbidden("Can't delete audits in production")
     election = get_or_404(Election, election_id)
     db_session.delete(election)
     db_session.commit()
