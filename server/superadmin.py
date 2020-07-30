34bf29a08265e29b6275ad5176b2da5fa1bc888b
Jonah Kagan <jonah@voting.works>
2020-07-29 17:21:36 -0700

    Superadmin: add button to delete election (#659)
    * Superadmin: add button to delete election
    
    * Disallow deleting audits in production

--- server/superadmin.py
+++ server/superadmin.py
@@ -1,11 +1,14 @@
 from flask import render_template, redirect, request, Blueprint
+from werkzeug.exceptions import Forbidden
 
 from .models import *  # pylint: disable=wildcard-import
+from .database import db_session
 from .auth import (
     UserType,
     with_superadmin_access,
     set_loggedin_user,
 )
+from .config import FLASK_ENV
 
 superadmin = Blueprint("superadmin", __name__)
 
@@ -47,3 +50,14 @@ def superadmin_jurisdictionadmin_login():
     user_email = request.form["email"]
     set_loggedin_user(UserType.JURISDICTION_ADMIN, user_email)
     return redirect("/")
+
+
+@superadmin.route("/superadmin/delete-election/<election_id>", methods=["POST"])
+@with_superadmin_access
+def superadmin_delete_election(election_id: str):
+    if FLASK_ENV == "production":  # pragma: no cover
+        raise Forbidden("Can't delete audits in production")
+    election = get_or_404(Election, election_id)
+    db_session.delete(election)
+    db_session.commit()
+    return redirect("/superadmin/")
--- server/tests/api/test_superadmin.py
+++ server/tests/api/test_superadmin.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 import json, pytest
 from flask.testing import FlaskClient
 
@@ -6,23 +7,18 @@ from ...models import *  # pylint: disable=wildcard-import
 from ..helpers import *  # pylint: disable=wildcard-import
 
 
-SA_TEST_AA_EMAIL = "sa-test-aa-email@example.com"
-SA_TEST_JA_EMAIL = "sa-test-ja-email@example.com"
-
-
 @pytest.fixture()
 def organization_id() -> str:
-    org_id, _ = create_org_and_admin(user_email=SA_TEST_AA_EMAIL)
+    org_id, _ = create_org_and_admin(user_email=DEFAULT_AA_EMAIL)
     return org_id
 
 
 @pytest.fixture()
 def election_id(client: FlaskClient, organization_id) -> str:
-    set_logged_in_user(client, UserType.AUDIT_ADMIN, SA_TEST_AA_EMAIL)
+    set_logged_in_user(client, UserType.AUDIT_ADMIN, DEFAULT_AA_EMAIL)
     election_id = create_election(
         client, organization_id=organization_id, is_multi_jurisdiction=True
     )
-    clear_logged_in_user(client)
     return election_id
 
 
@@ -43,33 +39,67 @@ def test_superadmin_organizations(client: FlaskClient, organization_id):
     data = assert_superadmin_access(client, "/superadmin/")
     assert "Organizations" in data
     assert org.name in data
-    assert SA_TEST_AA_EMAIL in data
+    assert DEFAULT_AA_EMAIL in data
 
-    rv = client.post("/superadmin/auditadmin-login", data={"email": SA_TEST_AA_EMAIL})
+    rv = client.post("/superadmin/auditadmin-login", data={"email": DEFAULT_AA_EMAIL})
     assert rv.status_code == 302
 
     rv = client.get("/api/me")
     auth_data = json.loads(rv.data)
-    assert auth_data["email"] == SA_TEST_AA_EMAIL
+    assert auth_data["email"] == DEFAULT_AA_EMAIL
     assert auth_data["type"] == UserType.AUDIT_ADMIN
 
 
 def test_superadmin_jurisdictions(client: FlaskClient, election_id):
-    create_jurisdiction_and_admin(election_id=election_id, user_email=SA_TEST_JA_EMAIL)
+    create_jurisdiction_and_admin(election_id=election_id, user_email=DEFAULT_JA_EMAIL)
 
     data = assert_superadmin_access(
         client, f"/superadmin/jurisdictions?election_id={election_id}"
     )
 
     assert "Jurisdictions" in data
-    assert SA_TEST_JA_EMAIL in data
+    assert DEFAULT_JA_EMAIL in data
 
     rv = client.post(
-        "/superadmin/jurisdictionadmin-login", data={"email": SA_TEST_JA_EMAIL}
+        "/superadmin/jurisdictionadmin-login", data={"email": DEFAULT_JA_EMAIL}
     )
     assert rv.status_code == 302
 
     rv = client.get("/api/me")
     auth_data = json.loads(rv.data)
-    assert auth_data["email"] == SA_TEST_JA_EMAIL
+    assert auth_data["email"] == DEFAULT_JA_EMAIL
     assert auth_data["type"] == UserType.JURISDICTION_ADMIN
+
+
+def test_superadmin_delete_election(
+    client: FlaskClient,
+    organization_id: str,
+    election_id: str,
+    round_1_id: str,  # pylint: disable=unused-argument
+):
+    election_id_2 = create_election(
+        client, audit_name="Audit 2", organization_id=organization_id
+    )
+
+    set_superadmin(client)
+    rv = client.post(f"/superadmin/delete-election/{election_id}")
+    assert rv.status_code == 302
+    assert urlparse(rv.headers["location"]).path == "/superadmin/"
+
+    assert Election.query.get(election_id) is None
+    assert Election.query.get(election_id_2) is not None
+
+    for model in [
+        AuditBoard,
+        BallotInterpretation,
+        Batch,
+        Contest,
+        ContestChoice,
+        Jurisdiction,
+        Round,
+        RoundContest,
+        RoundContestResult,
+        SampledBallot,
+        SampledBallotDraw,
+    ]:
+        assert model.query.count() == 0
