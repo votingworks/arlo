84ab8407af22544e4b082a93bc6235d94d9e4ab6
Morgan Love <morgan.j.love@gmail.com>
2020-08-06 15:34:26 -0700

    initial tests

--- client/src/components/MultiJurisdictionAudit/RoundManagement/RoundDataEntry.test.tsx
+++ client/src/components/MultiJurisdictionAudit/RoundManagement/RoundDataEntry.test.tsx
@@ -1,14 +1,43 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { useParams } from 'react-router-dom'
 import RoundDataEntry from './RoundDataEntry'
-import { roundMocks } from '../_mocks'
+import { roundMocks, contestMocks } from '../_mocks'
+import { renderWithRouter, withMockFetch } from '../../testUtilities'
+import { IContest } from '../../../types'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+  useRouteMatch: jest.fn(),
+  useParams: jest.fn(),
+}))
+const paramsMock = useParams as jest.Mock
+paramsMock.mockReturnValue({
+  electionId: '1',
+  jurisdictionId: '1',
+})
+
+const apiCalls = {
+  getJAContests: (response: { contests: IContest[] }) => ({
+    url: `/api/election/1/jurisdiction/1/contest`,
+    response,
+  }),
+}
 
 describe('offline round data entry', () => {
-  it('renders', () => {
-    // TODO code offline data entry
-    const { container } = render(
-      <RoundDataEntry round={roundMocks.singleIncomplete[0]} />
-    )
-    expect(container).toMatchSnapshot()
+  it('renders', async () => {
+    const expectedCalls = [
+      apiCalls.getJAContests({ contests: contestMocks.oneTargeted }),
+    ]
+    await withMockFetch(expectedCalls, async () => {
+      const { container } = renderWithRouter(
+        <RoundDataEntry round={roundMocks.singleIncomplete[0]} />,
+        {
+          route: '/election/1/jurisdiction/1',
+        }
+      )
+      await screen.findByText('Round 1 Data Entry')
+      expect(container).toMatchSnapshot()
+    })
   })
 })
