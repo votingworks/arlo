fb216960ead33c3226df24b76347e356acd0a55b
Jonah Kagan <jonah@voting.works>
2020-08-25 14:14:22 -0700

    Use snapshots in test_sampler.py

--- server/tests/audit_math/test_sampler.py
+++ server/tests/audit_math/test_sampler.py
@@ -40,7 +40,7 @@ def macro_contest():
     return Contest(name, info_dict)
 
 
-def test_draw_sample():
+def test_draw_sample(snapshot):
     # Test getting a sample
     manifest = {
         "pct 1": 25,
@@ -50,15 +50,10 @@ def test_draw_sample():
     }
 
     sample = sampler.draw_sample(SEED, manifest, 20, 0)
-
-    for i, item in enumerate(sample):
-        expected = expected_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
+    snapshot.assert_match(sample)
 
 
-def test_draw_more_samples():
+def test_draw_more_samples(snapshot):
     # Test getting a sample
     manifest = {
         "pct 1": 25,
@@ -67,71 +62,32 @@ def test_draw_more_samples():
         "pct 4": 25,
     }
 
-    samp_size = 10
     sample = sampler.draw_sample(SEED, manifest, 10, 0)
-    assert samp_size == len(sample), "Received sample of size {}, expected {}".format(
-        samp_size, len(sample)
-    )
+    snapshot.assert_match(sample)
 
-    for i, item in enumerate(sample):
-        expected = expected_first_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
-
-    samp_size = 10
     sample = sampler.draw_sample(SEED, manifest, 10, num_sampled=10)
-    assert samp_size == len(sample), "Received sample of size {}, expected {}".format(
-        samp_size, len(sample)
-    )
-    for i, item in enumerate(sample):
-        expected = expected_second_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
+    snapshot.assert_match(sample)
 
 
-def test_draw_macro_sample(macro_batches, macro_contest):
+def test_draw_macro_sample(macro_batches, macro_contest, snapshot):
     # Test getting a sample
     sample = sampler.draw_ppeb_sample(
         SEED, macro_contest, 10, 0, batch_results=macro_batches
     )
-
-    for i, item in enumerate(sample):
-        expected = expected_macro_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
+    snapshot.assert_match(sample)
 
 
-def test_draw_more_macro_sample(macro_batches, macro_contest):
+def test_draw_more_macro_sample(macro_batches, macro_contest, snapshot):
     # Test getting a sample
-    samp_size = 5
     sample = sampler.draw_ppeb_sample(
-        SEED, macro_contest, samp_size, 0, batch_results=macro_batches,
+        SEED, macro_contest, 5, 0, batch_results=macro_batches,
     )
-    assert samp_size == len(sample), "Received sample of size {}, expected {}".format(
-        samp_size, len(sample)
-    )
+    snapshot.assert_match(sample)
 
-    for i, item in enumerate(sample):
-        expected = expected_first_macro_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
-
-    samp_size = 5
     sample = sampler.draw_ppeb_sample(
-        SEED, macro_contest, samp_size, num_sampled=5, batch_results=macro_batches
+        SEED, macro_contest, 5, num_sampled=5, batch_results=macro_batches
     )
-    assert samp_size == len(sample), "Received sample of size {}, expected {}".format(
-        samp_size, len(sample)
-    )
-    for i, item in enumerate(sample):
-        expected = expected_second_macro_sample[i]
-        assert item == expected, "Draw sample failed: got {}, expected {}".format(
-            item, expected
-        )
+    snapshot.assert_match(sample)
 
 
 def random_manifest():
@@ -145,82 +101,3 @@ def test_ballot_labels():
         sample = sampler.draw_sample(SEED, manifest, 100, 0)
         for (_, (batch, ballot_number), _) in sample:
             assert 1 <= ballot_number <= manifest[batch]
-
-
-expected_sample = [
-    ("0.000617786129909912", ("pct 2", 3), 1),
-    ("0.002991631653037245", ("pct 3", 24), 1),
-    ("0.012057030610635061", ("pct 1", 25), 1),
-    ("0.017930028930651931", ("pct 4", 19), 1),
-    ("0.025599454926985137", ("pct 3", 15), 1),
-    ("0.045351055354441163", ("pct 1", 7), 1),
-    ("0.063913979803461405", ("pct 1", 8), 1),
-    ("0.064553852798863609", ("pct 1", 22), 1),
-    ("0.078998835671540970", ("pct 1", 20), 1),
-    ("0.090240829778172783", ("pct 3", 12), 1),
-    ("0.096136506157297637", ("pct 1", 20), 2),
-    ("0.104280162683637014", ("pct 4", 17), 1),
-    ("0.108948480696023984", ("pct 1", 25), 2),
-    ("0.111195681310332785", ("pct 1", 4), 1),
-    ("0.114438612046531251", ("pct 4", 3), 1),
-    ("0.130457464320709301", ("pct 2", 1), 1),
-    ("0.133484785501449819", ("pct 1", 12), 1),
-    ("0.134519219670087860", ("pct 3", 20), 1),
-    ("0.135840440920085144", ("pct 3", 10), 1),
-    ("0.138772253094235762", ("pct 4", 20), 1),
-]
-
-expected_macro_sample = [
-    ("0.003875995", "pct 16", 1),
-    ("0.011835450", "pct 2", 1),
-    ("0.022865957", "pct 2", 2),
-    ("0.035732442", "pct 3", 1),
-    ("0.125751540", "pct 2", 3),
-    ("0.136070319", "pct 18", 1),
-    ("0.150306323", "pct 6", 1),
-    ("0.176060218", "pct 4", 1),
-    ("0.183200120", "pct 13", 1),
-    ("0.191343085", "pct 18", 2),
-]
-
-expected_first_sample = [
-    ("0.000617786129909912", ("pct 2", 3), 1),
-    ("0.002991631653037245", ("pct 3", 24), 1),
-    ("0.012057030610635061", ("pct 1", 25), 1),
-    ("0.017930028930651931", ("pct 4", 19), 1),
-    ("0.025599454926985137", ("pct 3", 15), 1),
-    ("0.045351055354441163", ("pct 1", 7), 1),
-    ("0.063913979803461405", ("pct 1", 8), 1),
-    ("0.064553852798863609", ("pct 1", 22), 1),
-    ("0.078998835671540970", ("pct 1", 20), 1),
-    ("0.090240829778172783", ("pct 3", 12), 1),
-]
-
-expected_second_sample = [
-    ("0.096136506157297637", ("pct 1", 20), 2),
-    ("0.104280162683637014", ("pct 4", 17), 1),
-    ("0.108948480696023984", ("pct 1", 25), 2),
-    ("0.111195681310332785", ("pct 1", 4), 1),
-    ("0.114438612046531251", ("pct 4", 3), 1),
-    ("0.130457464320709301", ("pct 2", 1), 1),
-    ("0.133484785501449819", ("pct 1", 12), 1),
-    ("0.134519219670087860", ("pct 3", 20), 1),
-    ("0.135840440920085144", ("pct 3", 10), 1),
-    ("0.138772253094235762", ("pct 4", 20), 1),
-]
-
-expected_first_macro_sample = [
-    ("0.003875995", "pct 16", 1),
-    ("0.011835450", "pct 2", 1),
-    ("0.022865957", "pct 2", 2),
-    ("0.035732442", "pct 3", 1),
-    ("0.125751540", "pct 2", 3),
-]
-
-expected_second_macro_sample = [
-    ("0.136070319", "pct 18", 1),
-    ("0.150306323", "pct 6", 1),
-    ("0.176060218", "pct 4", 1),
-    ("0.183200120", "pct 13", 1),
-    ("0.191343085", "pct 18", 2),
-]
