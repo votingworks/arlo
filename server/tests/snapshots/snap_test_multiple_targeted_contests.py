c01d42565100569af7e5b25a065c2c3b6ca70c8e
Matt Bernhard <umbernhard@gmail.com>
2020-09-01 17:17:38 -0400

    Updated snapshots

--- server/tests/snapshots/snap_test_multiple_targeted_contests.py
+++ server/tests/snapshots/snap_test_multiple_targeted_contests.py
@@ -938,18 +938,3 @@ J2,4,37,,"Round 2: 0.654538594313269441, 0.852534572705101130",AUDITED,,Yes,\r
 J2,4,39,,Round 2: 0.616135113631286380,AUDITED,,Yes,\r
 J2,4,40,,"Round 2: 0.784605765918061241, 0.815960608185880613, 0.832024249630399283",AUDITED,,Yes,\r
 '''
-
-snapshots["test_sample_size_round_1 1"] = {
-    "Contest 1": [
-        {"key": "asn", "prob": 0.52, "size": 191},
-        {"key": "0.7", "prob": 0.7, "size": 295},
-        {"key": "0.8", "prob": 0.8, "size": 391},
-        {"key": "0.9", "prob": 0.9, "size": 562},
-    ],
-    "Contest 2": [
-        {"key": "asn", "prob": 0.51, "size": 485},
-        {"key": "0.7", "prob": 0.7, "size": 770},
-        {"key": "0.8", "prob": 0.8, "size": 1018},
-        {"key": "0.9", "prob": 0.9, "size": 1468},
-    ],
-}
