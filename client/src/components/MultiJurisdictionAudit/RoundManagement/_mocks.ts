6e082cbb9a1411c5fcb2ecfdc89baea858550f7d
Morgan Love <morgan.j.love@gmail.com>
2020-09-04 14:15:58 -0700

    covering BatchRoundDataEntry

--- client/src/components/MultiJurisdictionAudit/RoundManagement/_mocks.ts
+++ client/src/components/MultiJurisdictionAudit/RoundManagement/_mocks.ts
@@ -1,3 +1,5 @@
+import { IBatch } from './useBatchResults'
+
 export interface INullResultValues {
   [contestId: string]: {
     [choiceId: string]: null | string | number
@@ -26,6 +28,80 @@ export const resultsMocks: {
       'choice-id-3': 1,
       'choice-id-4': 2,
     },
+  },
+}
+
+export const batchResultsMocks: {
+  [key in 'complete']: INullResultValues
+} = {
+  complete: {
+    'batch-1': {
+      'choice-id-1': 1,
+      'choice-id-2': 2,
+    },
+    'batch-2': {
+      'choice-id-1': 1,
+      'choice-id-2': 2,
+    },
+    'batch-3': {
+      'choice-id-1': 1,
+      'choice-id-2': 2,
+    },
+  },
+}
+
+export const batchesMocks: {
+  [key in 'emptyInitial' | 'complete']: { batches: IBatch[] }
+} = {
+  emptyInitial: {
+    batches: [
+      {
+        id: 'batch-1',
+        name: 'Batch One',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+      {
+        id: 'batch-2',
+        name: 'Batch Two',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+      {
+        id: 'batch-3',
+        name: 'Batch Three',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+    ],
+  },
+  complete: {
+    batches: [
+      {
+        id: 'batch-1',
+        name: 'Batch One',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+      {
+        id: 'batch-2',
+        name: 'Batch Two',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+      {
+        id: 'batch-3',
+        name: 'Batch Three',
+        numBallots: 100,
+        auditBoard: null,
+        results: null,
+      },
+    ],
   },
 }
 
