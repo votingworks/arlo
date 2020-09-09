0ac5aed05a3af656cf4cdd4401bda27a026a0802
Matt Bernhard <umbernhard@gmail.com>
2020-09-09 15:56:53 -0400

    More delegation tweaks, also changed cypress script to exit rather than interrupt

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -3,8 +3,7 @@
 export ARLO_AUDITADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 export ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 
-trap 'kill 0' SIGINT SIGHUP EXIT
+trap 'exit 0' SIGINT SIGHUP EXIT
 cd "$(dirname "${BASH_SOURCE[0]}")"
 FLASK_ENV=test ../run-dev.sh &
 yarn run cypress run 
-exit 0
