19021eaf025ee2457785598c0b29960d7290efad
Matt Bernhard <umbernhard@gmail.com>
2020-09-14 11:34:07 -0400

    REstoring cypress trap

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -3,7 +3,7 @@
 export ARLO_AUDITADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 export ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
 
-trap 'exit 0' SIGINT SIGHUP EXIT
+trap 'kill 0' SIGINT SIGHUP EXIT
 cd "$(dirname "${BASH_SOURCE[0]}")"
 FLASK_ENV=test ../run-dev.sh &
 yarn run cypress run 
