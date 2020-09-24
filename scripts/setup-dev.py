de4e9338d70659aa176e73fa25719bba8ddf8d5b
Matt Bernhard <umbernhard@gmail.com>
2020-09-24 13:59:52 -0400

    Tweaked some features of the script, implemented requested changes

--- scripts/setup-dev.py
+++ scripts/setup-dev.py
@@ -14,83 +14,111 @@ try:
 
     print("Running script to setup local Arlo instance...")
 
-    res = input("Do you have an instance of nOAuth installed? [y/N]")
+    res = input("Do you have an instance of nOAuth configured? [y/N]")
 
     if res != "y":
-        res = input("Would you like to install nOAuth? [Y/n]")
 
-        if res == "n":
-            print("Please install nOAuth instance before running this script again.")
-            sys.exit(0)
-
-        d = input("Where would you like nOAuth installed? [default: .]")
-
-        if not d:
-            d = "."
-
-        d = os.path.expanduser(d)
-
-        if not os.path.exists(d):
-            os.makedirs(d)
-
-        os.chdir(d)
-
-        command = "git clone https://github.com/votingworks/nOAuth.git"
-
-        subprocess.run([command], shell=True, check=True)
-
-        noAuthPath = d
-
-    while True:
-        if not noAuthPath:
-            d = input("Where is nOAuth located?")
-
-        d = os.path.expanduser(d)
-        if os.path.exists(d):
-            noAuthPath = d
-            break
-
-        print("That path does not exist.")
-        d = ""
-
-    os.chdir(noAuthPath)
-
-    res = input("Would you like to run nOAuth? [Y/n]")
-    if res == "n":
-        print("Please see README for running Arlo with custom nOAuth.")
-        sys.exit(0)
-
-    port = ""
-    while True:
-        res = input("What port should nOAuth be run on? [8080]")
+        res = input("Would you like to use VotingWorks' instance of nOAuth? [Y/n]")
 
         if not res:
-            res = "8080"
+            res = "y"
 
-        try:
-            int(res)
-            port = res
-            break
-        except ValueError:
-            print('"{}" is not a valid port.'.format(res))
+        nOAuthAddr = "https://votingworks-noauth.herokuapp.com"
+        if res not in ["Y", "y"]:
 
-    print("Running nOAuth on port {}".format(res))
-    command = "PORT=8080 pipenv run python app.py"
-    # pylint: disable=subprocess-popen-preexec-fn
-    child = subprocess.Popen(
-        [command],
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        preexec_fn=os.setsid,
+            res = input("Would you like to install nOAuth? [Y/n]")
+
+            if res == "n":
+                print(
+                    "Please install nOAuth instance before running this script again."
+                )
+                sys.exit(0)
+
+            d = input("Where would you like nOAuth installed? [default: .]")
+
+            if not d:
+                d = "."
+
+            d = os.path.expanduser(d)
+
+            if not os.path.exists(d):
+                os.makedirs(d)
+
+            os.chdir(d)
+
+            command = "git clone https://github.com/votingworks/nOAuth.git"
+
+            subprocess.run([command], shell=True, check=True)
+
+            noAuthPath = d
+
+            while True:
+                if not noAuthPath:
+                    d = input("Where is nOAuth located?")
+
+                d = os.path.expanduser(d)
+                if os.path.exists(d):
+                    noAuthPath = d
+                    break
+
+                print("That path does not exist.")
+                d = ""
+
+            os.chdir(noAuthPath)
+
+            res = input("Would you like to run nOAuth? [Y/n]")
+            if res == "n":
+                print("Please see README for running Arlo with custom nOAuth.")
+                sys.exit(0)
+
+            port = ""
+            while True:
+                res = input("What port should nOAuth be run on? [8080]")
+
+                if not res:
+                    res = "8080"
+
+                try:
+                    int(res)
+                    port = res
+                    break
+                except ValueError:
+                    print('"{}" is not a valid port.'.format(res))
+
+            print("Running nOAuth on port {}".format(res))
+            command = "PORT=8080 pipenv run python app.py"
+            # pylint: disable=subprocess-popen-preexec-fn
+            child = subprocess.Popen(
+                [command],
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                preexec_fn=os.setsid,
+            )
+
+            children.append(child)
+            nOAuthAddr = "http://localhost:{}".format(port)
+
+    print("Configuring arlo environment")
+    os.environ["ARLO_AUDITADMIN_AUTH0_BASE_URL"] = nOAuthAddr
+    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL"] = nOAuthAddr
+
+    os.environ["ARLO_AUDITADMIN_AUTH0_CLIENT_ID"] = "test"
+    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_CLIENT_ID"] = "test"
+
+    os.environ["ARLO_AUDITADMIN_AUTH0_CLIENT_SECRET"] = "secret"
+    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_CLIENT_SECRET"] = "secret"
+
+    res = input(
+        "Would you like to install the Arlo dev environment?\n"
+        + "If this is your first run of Arlo after install, you should do this. [y/N]"
     )
 
-    children.append(child)
-
-    res = input("Would you like to install the Arlo dev environment? [y/N]")
-
     if res == "y":
-        subprocess.run(["make dev-environment"], check=True)
+        subprocess.run(["make dev-environment"], check=True, shell=True)
+
+    print("Migrating the database to the newset data model")
+    subprocess.run(["alembic upgrade head"], check=True, shell=True)
 
     orgname = input("What is the name of your test organization? [test]")
 
@@ -121,18 +149,6 @@ try:
         shell=True,
         check=True,
     )
-
-    print("Configuring arlo environment")
-    os.environ["ARLO_AUDITADMIN_AUTH0_BASE_URL"] = "http://localhost:{}".format(port)
-    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL"] = "http://localhost:{}".format(
-        port
-    )
-
-    os.environ["ARLO_AUDITADMIN_AUTH0_CLIENT_ID"] = "test"
-    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_CLIENT_ID"] = "test"
-
-    os.environ["ARLO_AUDITADMIN_AUTH0_CLIENT_SECRET"] = "secret"
-    os.environ["ARLO_JURISDICTIONADMIN_AUTH0_CLIENT_SECRET"] = "secret"
 
     print("Running Arlo. Happy auditing!")
     subprocess.run(["./run-dev.sh"], check=True)
