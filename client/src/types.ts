70b23986bf6ec987fbdd43a40c1a6ba63675e3cc
Matt Bernhard <umbernhard@gmail.com>
2020-10-30 12:39:00 -0400

    Added frontend for math selection

--- client/src/types.ts
+++ client/src/types.ts
@@ -178,5 +178,6 @@ export interface IAuditSettings {
   randomSeed: string | null
   riskLimit: number | null
   auditType: 'BALLOT_POLLING' | 'BATCH_COMPARISON' | 'BALLOT_COMPARISON'
+  ballotPollingType: 'BRAVO' | 'MINERVA'
   auditName: string
 }
