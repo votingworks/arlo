11095ddea3e51fd5b24739647d6d53c2c93edc4c
Matt Bernhard <umbernhard@gmail.com>
2020-11-03 11:30:23 -0500

    Added validation for math types

--- server/models.py
+++ server/models.py
@@ -45,7 +45,7 @@ class Election(BaseModel):
     # audit_name must be unique within each Organization
     audit_name = Column(String(200), nullable=False)
     audit_type = Column(Enum(AuditType), nullable=False)
-    audit_math_type = Column(Enum(AuditMathType), nullable=True)
+    audit_math_type = Column(Enum(AuditMathType), nullable=False)
     # election_name can be the same across audits
     election_name = Column(String(200))
     state = Column(String(100))
