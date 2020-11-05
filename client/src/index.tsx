1285bb103222ded452ba786c6e52a1da7bb20f7a
Jonah Kagan <jonah@voting.works>
2020-11-05 14:28:41 -0800

    Sentry frontend (#865)
    * Add Sentry to frontend
    
    Intializes error capture by Sentry when REACT_APP_SENTRY_DSN env
    variable is set.
    
    * Add environment

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,19 @@ import 'react-app-polyfill/ie11'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+console.log(process.env)
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <Router>
