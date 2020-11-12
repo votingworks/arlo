167fa91abcd3fdaaa7ca40396009062df1a48dcc
Ben Adida <ben@adida.net>
2020-11-11 21:30:40 -0800

    add test... but this test doesn't yet fail on old code so... hmm

--- server/tests/util/test_jurisdiction_bulk_update.py
+++ server/tests/util/test_jurisdiction_bulk_update.py
@@ -1,3 +1,5 @@
+from flask.testing import FlaskClient
+from ...auth import UserType
 from ...util.jurisdiction_bulk_update import bulk_update_jurisdictions
 from ...models import *  # pylint: disable=wildcard-import
 from ...database import db_session
@@ -54,3 +56,33 @@ def test_remove_outdated_jurisdictions(election_id):
     assert new_admins == []
     assert User.query.filter_by(email="ja2@ca.gov").first()  # keep the user
     assert Jurisdiction.query.filter_by(election_id=election_id).count() == 0
+
+
+def test_dont_clobber_other_elections(client: FlaskClient, election_id, org_id):
+    election = Election.query.get(election_id)
+
+    set_logged_in_user(client, UserType.AUDIT_ADMIN, user_key=DEFAULT_AA_EMAIL)
+    other_election_id = create_election(
+        client, audit_name="Test Audit 2", organization_id=org_id
+    )
+    other_election = Election.query.get(other_election_id)
+
+    # Add jurisdictions.
+    bulk_update_jurisdictions(db_session, election, [("Jurisdiction #1", "ja2@ca.gov")])
+    db_session.commit()
+
+    # Add jurisdictions for other election
+    bulk_update_jurisdictions(
+        db_session, other_election, [("Jurisdiction #2", "ja3@ca.gov")]
+    )
+    db_session.commit()
+
+    # make sure first election admins were not clobbered
+    assert (
+        len(
+            Jurisdiction.query.filter_by(election_id=election.id)[
+                0
+            ].jurisdiction_administrations
+        )
+        == 1
+    )
