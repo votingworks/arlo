0a116b7450eaa8dc003be562d6f942d7ea7ae8c6
Jonah Kagan <jonah@voting.works>
2020-11-17 16:06:32 -0800

    Visual PDF snapshots
    Use pdf-visual-diff to create image snapshots for label/placeholder
    pdfs.

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,6 +2,7 @@
 
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-canvas-mock'
+import 'pdf-visual-diff/lib/toMatchPdfSnapshot'
 
 global.document.createRange = () => ({
   setStart: () => {},
