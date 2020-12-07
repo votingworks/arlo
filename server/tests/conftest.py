ee02693d9f25593472f2209fae2391081266ad9a
Jonah Kagan <jonah@voting.works>
2020-12-07 13:03:48 -0800

    Add election id to bgcompute call for ballot manifests in conftest

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -185,7 +185,7 @@ def manifests(client: FlaskClient, election_id: str, jurisdiction_ids: List[str]
         },
     )
     assert_ok(rv)
-    bgcompute_update_ballot_manifest_file()
+    bgcompute_update_ballot_manifest_file(election_id)
 
 
 @pytest.fixture
