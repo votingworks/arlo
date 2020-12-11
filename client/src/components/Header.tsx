ab956231e7584ba7c0e4a3f5e810f6968a44fc9d
Jonah Kagan <jonah@voting.works>
2020-12-10 17:56:40 -0800

    start on banner

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,15 +6,27 @@ import {
   NavbarHeading,
   Alignment,
   NavbarDivider,
+  Tag,
+  Button,
+  Menu,
+  MenuItem,
+  Popover,
+  Position,
+  Colors,
 } from '@blueprintjs/core'
 import { Link, useRouteMatch, RouteComponentProps } from 'react-router-dom'
 import { useAuthDataContext } from './UserContext'
 import FormButton from './Atoms/Form/FormButton'
 import { Inner } from './Atoms/Wrapper'
+import LinkButton from './Atoms/LinkButton'
 
-const ButtonBar = styled.div`
-  display: inline-block;
-  margin-right: 10px;
+const SupportBar = styled(Navbar)`
+  background-color: ${Colors.ORANGE5};
+  height: 30px;
+  padding: 0;
+  .bp3-navbar-group {
+    height: 30px;
+  }
 `
 
 const Nav = styled(Navbar)`
@@ -25,10 +37,18 @@ const Nav = styled(Navbar)`
     height: 35px;
     padding-top: 5px;
   }
+`
 
-  .bp3-navbar-divider {
-    margin-right: 15px;
-    margin-left: 0;
+const UserMenu = styled.div`
+  .bp3-button {
+    width: 200px;
+  }
+  .bp3-button-text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .bp3-menu {
+    width: 200px;
   }
 `
 
@@ -61,56 +81,87 @@ const Header: React.FC<{}> = () => {
       j => j.id === jurisdictionMatch.params.jurisdictionId
     )
   return (
-    <Nav>
-      <InnerBar>
-        <NavbarGroup align={Alignment.LEFT}>
-          <NavbarHeading>
-            <Link to="/">
-              <img src="/arlo.png" alt="Arlo, by VotingWorks" />
-            </Link>
-          </NavbarHeading>
-          {jurisdiction && (
-            <NavbarHeading>Jurisdiction: {jurisdiction.name}</NavbarHeading>
+    <>
+      {auth && auth.superadminUser && (
+        <SupportBar>
+          <InnerBar>
+            <NavbarGroup align={Alignment.LEFT}>
+              <NavbarHeading>Support Tools</NavbarHeading>
+            </NavbarGroup>
+            <NavbarGroup align={Alignment.RIGHT}>
+              <NavbarHeading>{auth.superadminUser.email}</NavbarHeading>
+            </NavbarGroup>
+          </InnerBar>
+        </SupportBar>
+      )}
+      <Nav>
+        <InnerBar>
+          <NavbarGroup align={Alignment.LEFT}>
+            <NavbarHeading>
+              <Link to="/">
+                <img src="/arlo.png" alt="Arlo, by VotingWorks" />
+              </Link>
+            </NavbarHeading>
+            {jurisdiction && (
+              <NavbarHeading>Jurisdiction: {jurisdiction.name}</NavbarHeading>
+            )}
+          </NavbarGroup>
+          {auth && auth.user && (
+            <NavbarGroup align={Alignment.RIGHT}>
+              {electionId && auth.user.type === 'audit_admin' && (
+                <>
+                  <LinkButton
+                    to={`/election/${electionId}/setup`}
+                    minimal
+                    icon="wrench"
+                  >
+                    Audit Setup
+                  </LinkButton>
+                  <LinkButton
+                    to={`/election/${electionId}/progress`}
+                    minimal
+                    icon="horizontal-bar-chart"
+                  >
+                    Audit Progress
+                  </LinkButton>
+                  <LinkButton to="/" minimal icon="projects">
+                    View Audits
+                  </LinkButton>
+                  <LinkButton to="/" minimal icon="insert">
+                    New Audit
+                  </LinkButton>
+                  <NavbarDivider />
+                </>
+              )}
+              {auth.user.type !== 'audit_board' && (
+                <UserMenu>
+                  <Popover
+                    content={
+                      <Menu>
+                        <MenuItem
+                          text="Log out"
+                          onClick={() =>
+                            window.location.replace('/auth/logout')
+                          }
+                        />
+                      </Menu>
+                    }
+                    usePortal={false}
+                    position={Position.BOTTOM}
+                    minimal
+                    fill
+                  >
+                    <Button icon="user" minimal>
+                      {auth.user.email}
+                    </Button>
+                  </Popover>
+                </UserMenu>
+              )}
+            </NavbarGroup>
           )}
-        </NavbarGroup>
-        <NavbarGroup align={Alignment.RIGHT}>
-          {electionId && auth && auth.user && auth.user.type === 'audit_admin' && (
-            <>
-              <NavbarHeading>
-                <Link to={`/election/${electionId}/setup`}>Audit Setup</Link>
-              </NavbarHeading>
-              <NavbarDivider />
-              <NavbarHeading>
-                <Link to={`/election/${electionId}/progress`}>
-                  Audit Progress
-                </Link>
-              </NavbarHeading>
-              <NavbarDivider />
-              <NavbarHeading>
-                <Link to="/">View Audits</Link>
-              </NavbarHeading>
-              <NavbarDivider />
-              <NavbarHeading>
-                <Link to="/">New Audit</Link>
-              </NavbarHeading>
-            </>
-          )}
-          <ButtonBar id="reset-button-wrapper" />
-          {/* istanbul ignore next */
-          auth && auth.user && (
-            <FormButton
-              size="sm"
-              onClick={
-                /* istanbul ignore next */
-                () => window.location.replace('/auth/logout')
-              }
-            >
-              Log out
-            </FormButton>
-          )}
-        </NavbarGroup>
-      </InnerBar>
-    </Nav>
+        </InnerBar>
+      </Nav>
+    </>
   )
 }
 
