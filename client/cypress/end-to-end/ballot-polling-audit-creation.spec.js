e3250eb7f1829aa1cd25e9a4d508dc368fe20289
Morgan Love <morgan.j.love@gmail.com>
2021-01-19 15:17:40 -0800

    adding ballot polling tests

--- client/cypress/end-to-end/ballot-polling-audit-creation.spec.js
+++ client/cypress/end-to-end/ballot-polling-audit-creation.spec.js
@@ -33,4 +33,77 @@ describe('Audit creation, filling in standard ballot polling values', () => {
     cy.findByText('Back').click()
     cy.findAllByText('Audit Settings').should('have.length', 2)
   })
+
+  describe('Creating an offline audit', () => {
+    it('AA sets up audit', () => {
+      cy.fixture('CSVs/jurisdiction/sample_jurisdiction_filesheet.csv').then(fileContent => {
+        cy.get('input[type="file"]').first().attachFile({
+          fileContent: fileContent.toString(),
+          fileName: 'sample_jurisdiction_filesheet.csv',
+          mimeType: 'csv'
+        })
+      })
+      cy.findAllByText('Upload File').spread((firstButton, secondButton) => {
+        firstButton.click()
+      })
+      cy.contains("Upload successfully completed")   
+  
+      cy.wait(100) // gets stuck in an infinite loop without a 100ms wait here
+      cy.findByText('Next').click()
+      cy.get('input[name="contests[0].name"]').type('Contest')
+      cy.get('input[name="contests[0].choices[0].name"]').type('A')
+      cy.get('input[name="contests[0].choices[0].numVotes"]').type('300')
+      cy.get('input[name="contests[0].choices[1].name"]').type('B')
+      cy.get('input[name="contests[0].choices[1].numVotes"]').type('100')
+      cy.get('input[name="contests[0].totalBallotsCast"]').type('400')
+      cy.findByText('Select Jurisdictions').click()
+      cy.findByLabelText('Death Star').check({ force: true })
+      cy.findByText('Save & Next').click()
+      cy.findAllByText('Opportunistic Contests').should('have.length', 2)
+      cy.findByText('Save & Next').click()
+      cy.get('#state').select('AL')
+      cy.get('input[name=electionName]').type(`Test Election`)
+      cy.get('#risk-limit').select('10')
+      cy.get('input[name=randomSeed]').type("543210")
+      cy.findByText('Save & Next').click()
+      cy.findAllByText('Review & Launch').should('have.length', 2)
+    })
+  })
+
+  describe('Creating an online audit', () => {
+    it('AA sets up audit', () => {
+      cy.fixture('CSVs/jurisdiction/sample_jurisdiction_filesheet.csv').then(fileContent => {
+        cy.get('input[type="file"]').first().attachFile({
+          fileContent: fileContent.toString(),
+          fileName: 'sample_jurisdiction_filesheet.csv',
+          mimeType: 'csv'
+        })
+      })
+      cy.findAllByText('Upload File').spread((firstButton, secondButton) => {
+        firstButton.click()
+      })
+      cy.contains("Upload successfully completed")   
+  
+      cy.wait(100) // gets stuck in an infinite loop without a 100ms wait here
+      cy.findByText('Next').click()
+      cy.get('input[name="contests[0].name"]').type('Contest')
+      cy.get('input[name="contests[0].choices[0].name"]').type('A')
+      cy.get('input[name="contests[0].choices[0].numVotes"]').type('300')
+      cy.get('input[name="contests[0].choices[1].name"]').type('B')
+      cy.get('input[name="contests[0].choices[1].numVotes"]').type('100')
+      cy.get('input[name="contests[0].totalBallotsCast"]').type('400')
+      cy.findByText('Select Jurisdictions').click()
+      cy.findByLabelText('Death Star').check({ force: true })
+      cy.findByText('Save & Next').click()
+      cy.findAllByText('Opportunistic Contests').should('have.length', 2)
+      cy.findByText('Save & Next').click()
+      cy.get('#state').select('AL')
+      cy.get('input[name=electionName]').type(`Test Election`)
+      cy.get("input[value=online]").click({ force: true })
+      cy.get('#risk-limit').select('10')
+      cy.get('input[name=randomSeed]').type("543210")
+      cy.findByText('Save & Next').click()
+      cy.findAllByText('Review & Launch').should('have.length', 2)
+    })
+  })
 })
