2ec44303f63f6c4f860e73df0fd2dc1b1f598f1a
Jonah Kagan <jonah@voting.works>
2021-01-20 13:14:27 -0800

    JA audit board status improvements (#1013)
    * JA audit board status improvements
    
    - Fix the status box to only show audit boards complete once they're
    signed off
    - Add tags/intent to the progress bars for individual audit boards in
    order to differentiate which boards have signed off

--- client/src/components/MultiJurisdictionAudit/RoundManagement/RoundProgress.tsx
+++ client/src/components/MultiJurisdictionAudit/RoundManagement/RoundProgress.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { ProgressBar, H4 } from '@blueprintjs/core'
+import { ProgressBar, H4, Tag, Intent } from '@blueprintjs/core'
 import styled from 'styled-components'
 import { IAuditBoard } from '../useAuditBoards'
 
 const MainBarWrapper = styled.div`
-  margin-bottom: 20px;
+  margin-bottom: 30px;
   width: 500px;
   font-size: 16px;
+  .bp3-progress-bar {
+    height: 12px;
+  }
 `
 
 const SmallBarWrapper = styled.div`
-  margin-bottom: 10px;
-  width: 400px;
+  margin-bottom: 30px;
+  width: 500px;
+  > div:first-child {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 5px;
+  }
+  .bp3-tag {
+    margin-left: 5px;
+  }
 `
 
 const RoundProgress = ({ auditBoards }: { auditBoards: IAuditBoard[] }) => {
@@ -23,17 +34,22 @@ const RoundProgress = ({ auditBoards }: { auditBoards: IAuditBoard[] }) => {
   const sampledBallots = sum(
     auditBoards.map(ab => ab.currentRoundStatus.numSampledBallots)
   )
+  const progressIntent = ((): Intent => {
+    if (auditedBallots === 0) return Intent.NONE
+    if (auditedBallots < sampledBallots) return Intent.PRIMARY
+    return Intent.SUCCESS
+  })()
   return (
     <>
       <MainBarWrapper>
         <H4>Audit Board Progress</H4>
-        <span>
+        <p>
           {auditedBallots} of {sampledBallots} ballots audited{' '}
-        </span>
+        </p>
         <ProgressBar
           value={auditedBallots / sampledBallots}
-          animate={auditedBallots < sampledBallots}
-          intent="primary"
+          intent={progressIntent}
+          stripes={false}
         />
       </MainBarWrapper>
       {auditBoards.map(
@@ -41,22 +57,35 @@ const RoundProgress = ({ auditBoards }: { auditBoards: IAuditBoard[] }) => {
           id,
           name,
           currentRoundStatus: { numAuditedBallots, numSampledBallots },
-        }) => (
-          <SmallBarWrapper key={id}>
-            {numSampledBallots > 0 ? (
-              <>
-                <span>{`${name}: ${numAuditedBallots} of ${numSampledBallots} ballots audited `}</span>
-                <ProgressBar
-                  value={numAuditedBallots / numSampledBallots}
-                  animate={numAuditedBallots < numSampledBallots}
-                  intent="primary"
-                />
-              </>
-            ) : (
-              <span>{name}: no ballots to audit</span>
-            )}
-          </SmallBarWrapper>
-        )
+          signedOffAt,
+        }) => {
+          const [status, intent] = ((): [string, Intent] => {
+            if (numAuditedBallots === 0) return ['Not started', Intent.NONE]
+            if (numAuditedBallots < numSampledBallots)
+              return ['In progress', Intent.PRIMARY]
+            if (!signedOffAt) return ['Not signed off', Intent.WARNING]
+            return ['Signed off', Intent.SUCCESS]
+          })()
+          return (
+            <SmallBarWrapper key={id}>
+              {numSampledBallots > 0 ? (
+                <>
+                  <div>
+                    <span>{`${name}: ${numAuditedBallots} of ${numSampledBallots} ballots audited `}</span>
+                    <Tag intent={intent}>{status}</Tag>
+                  </div>
+                  <ProgressBar
+                    value={numAuditedBallots / numSampledBallots}
+                    intent={intent}
+                    stripes={false}
+                  />
+                </>
+              ) : (
+                <>{name}: no ballots to audit</>
+              )}
+            </SmallBarWrapper>
+          )
+        }
       )}
     </>
   )
