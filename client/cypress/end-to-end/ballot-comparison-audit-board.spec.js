34376c98aeb5d6cc6f490ef39d41c5e8557c8ec6
Morgan Love <morgan.j.love@gmail.com>
2021-01-27 10:14:36 -0800

    unskipping tests

--- client/cypress/end-to-end/ballot-comparison-audit-board.spec.js
+++ client/cypress/end-to-end/ballot-comparison-audit-board.spec.js
@@ -2,7 +2,7 @@ import 'cypress-file-upload'
 
 before(() => cy.exec('./cypress/seed-test-db.sh'))
 
-describe.skip('Audit Boards', () => {
+describe('Audit Boards', () => {
   const uuid = () => Cypress._.random(0, 1e6)
   const id = 0
 
