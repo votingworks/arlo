4184b137c61c26b4d49e8aa2b98c114c1cbfd0a9
Morgan Love <morgan.j.love@gmail.com>
2021-02-16 10:27:00 -0800

    cleanup

--- client/cypress/end-to-end/ballot-polling-happy-path.spec.js
+++ client/cypress/end-to-end/ballot-polling-happy-path.spec.js
@@ -74,8 +74,7 @@ describe('Ballot Polling', () => {
     cy.findAllByText('Launch Audit').spread((firstButton, secondButton) => {
       secondButton.click()
     })
-    // cy.contains('Audit Progress')
-    cy.contains('Drawing a random sample of ballots...') // fail point?
+    cy.contains('Drawing a random sample of ballots...')
     cy.get('tbody').children('tr').its('length').should('be.gt', 0) // ensures ballot drawing is done
     cy.logout(auditAdmin)
     cy.loginJurisdictionAdmin(jurisdictionAdmin)
@@ -146,7 +145,6 @@ describe('Ballot Polling', () => {
     cy.findAllByText('Launch Audit').spread((firstButton, secondButton) => {
       secondButton.click()
     })
-    // cy.contains('Audit Progress')
     cy.contains('Drawing a random sample of ballots...')
     cy.get('tbody').children('tr').its('length').should('be.gt', 0) // ensures ballot drawing is done
     cy.logout(auditAdmin)
