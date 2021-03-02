67f4d5195fd87ac33159447dc02106b1da5bc989
Morgan Love <morgan.j.love@gmail.com>
2021-03-02 15:17:10 -0800

    tweaking x

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -6,4 +6,5 @@ export ARLO_JURISDICTIONADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuap
 trap 'kill 0' SIGINT SIGHUP EXIT
 cd "$(dirname "${BASH_SOURCE[0]}")"
 FLASK_ENV=test ../run-dev.sh &
+xvfb-run -s '-screen 0 1280x800x24'
 yarn run cypress run --browser chrome
