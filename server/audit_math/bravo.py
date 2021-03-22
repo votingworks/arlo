87f2de1805ceac9ca0cd351fd55e02221b06ef40
Matt Bernhard <umbernhard@gmail.com>
2021-03-22 20:02:33 +0000

    Swapping order of if statemnt to avoid error

--- server/audit_math/bravo.py
+++ server/audit_math/bravo.py
@@ -420,10 +420,9 @@ def get_sample_size(
     # large proportion (for large elections).
     large_election_threshold = 100000
     all_ballots_threshold = num_ballots * 0.25
-    if (
+    if p_w == p_l or (
         num_ballots > large_election_threshold
         and samples["0.9"]["size"] >= all_ballots_threshold
-        or p_w == p_l
     ):
         return {
             "all-ballots": {
