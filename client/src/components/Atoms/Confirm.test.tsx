401a89a69592516fe348670c0dc5ee6e67beff0e
Morgan Love <morgan.j.love@gmail.com>
2021-05-06 14:06:34 -0700

    refactoring tests

--- client/src/components/Atoms/Confirm.test.tsx
+++ client/src/components/Atoms/Confirm.test.tsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import { render, screen, within, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Intent } from '@blueprintjs/core'
-import { useConfirm, Confirm, IConfirmOptions } from './Confirm'
+import { Button } from '@blueprintjs/core'
+import { useConfirm, Confirm } from './Confirm'
 
-const confirmMock: IConfirmOptions = {
-  title: 'Confirm Component',
-  description: (
-    <div>
-      <p>
-        <strong>This action cannot be undone.</strong>
-      </p>
-    </div>
-  ),
-  yesButtonLabel: 'Yes',
-  yesButtonIntent: Intent.PRIMARY,
-  onYesClick: () =>
-    new Promise<void>(resolve => {
-      resolve()
-    }),
+const onYesClickMock = jest.fn().mockResolvedValue(true)
+
+const ConfirmConsumer = () => {
+  const { confirm, confirmProps } = useConfirm()
+  return (
+    <>
+      <Button
+        onClick={() =>
+          confirm({
+            title: 'Test Title',
+            description:
+              'This action cannot be undone. Do you want to continue?',
+            onYesClick: onYesClickMock,
+            yesButtonLabel: 'Yes',
+          })
+        }
+      >
+        Open Confirm
+      </Button>
+      <Confirm {...confirmProps} />
+    </>
+  )
 }
-
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useState: jest.fn(),
-}))
 
 afterEach(() => {
   jest.clearAllMocks()
@@ -32,63 +34,61 @@ afterEach(() => {
 
 describe('Confirm component', () => {
   it('opens confirm component', async () => {
-    const setOptions = jest.fn()
+    render(<ConfirmConsumer />)
 
-    // passing default values for state to open the component
-    // as state is mocked
-    const useStateMock: any = () => [confirmMock, setOptions]
-    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+    userEvent.click(screen.getByRole('button', { name: 'Open Confirm' }))
 
-    const { confirm: confirm1, confirmProps: confirmProps1 } = useConfirm()
-    confirm1(confirmMock)
-
-    expect(setOptions).toHaveBeenCalledTimes(1)
-
-    const { container } = render(<Confirm {...confirmProps1} />)
     const dialog = (await screen.findByRole('heading', {
-      name: /Confirm Component/,
+      name: /Test Title/,
     })).closest('.bp3-dialog')! as HTMLElement
-    within(dialog).getByText('This action cannot be undone.')
-    expect(container).toMatchSnapshot()
+    within(dialog).getByText(
+      'This action cannot be undone. Do you want to continue?'
+    )
   })
 
-  it('closes confirm component', async () => {
-    const setOptions = jest.fn()
+  it('closes confirm component when cancelled', async () => {
+    render(<ConfirmConsumer />)
 
-    // passing default values for state to open the component
-    // as state is mocked
-    const useStateMock: any = () => [confirmMock, setOptions]
-    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+    userEvent.click(screen.getByRole('button', { name: 'Open Confirm' }))
 
-    const { confirm: confirm1, confirmProps: confirmProps1 } = useConfirm()
-    confirm1(confirmMock)
-
-    expect(setOptions).toHaveBeenCalledTimes(1)
-
-    const { container, rerender } = render(<Confirm {...confirmProps1} />)
     const dialog = (await screen.findByRole('heading', {
-      name: /Confirm Component/,
+      name: /Test Title/,
     })).closest('.bp3-dialog')! as HTMLElement
-    within(dialog).getByText('This action cannot be undone.')
+    within(dialog).getByText(
+      'This action cannot be undone. Do you want to continue?'
+    )
+
     userEvent.click(within(dialog).getByRole('button', { name: 'Cancel' }))
-
-    // re-mocking state to update the values as
-    // state is mocked, setOptions won't update it to close component.
-    const useStateMock2: any = () => [null, setOptions]
-    jest.spyOn(React, 'useState').mockImplementation(useStateMock2)
-
-    const { confirm: confirm2, confirmProps: confirmProps2 } = useConfirm()
-    confirm2(null)
-
-    // re-rendering component to pass null values
-    // to not open the popup
-    rerender(<Confirm {...confirmProps2} />)
 
     await waitFor(() => {
       expect(
-        screen.queryByText('This action cannot be undone.')
+        screen.queryByText(
+          'This action cannot be undone. Do you want to continue?'
+        )
       ).not.toBeInTheDocument()
     })
-    expect(container).toMatchSnapshot()
+  })
+
+  it('closes confirm component when clicked on Yes', async () => {
+    render(<ConfirmConsumer />)
+
+    userEvent.click(screen.getByRole('button', { name: 'Open Confirm' }))
+
+    const dialog = (await screen.findByRole('heading', {
+      name: /Test Title/,
+    })).closest('.bp3-dialog')! as HTMLElement
+    within(dialog).getByText(
+      'This action cannot be undone. Do you want to continue?'
+    )
+
+    userEvent.click(within(dialog).getByRole('button', { name: 'Yes' }))
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText(
+          'This action cannot be undone. Do you want to continue?'
+        )
+      ).not.toBeInTheDocument()
+    })
   })
 })
