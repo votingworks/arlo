4be582f4723b27aaed700a3bccc7460c22d585c5
Jonah Kagan <jonah@voting.works>
2021-09-01 10:44:44 -0700

    add missing files

---
+++ server/tests/api/test_activity.py
@@ -0,0 +1,154 @@
+from typing import List
+from flask.testing import FlaskClient
+
+from ..helpers import *  # pylint: disable=wildcard-import
+
+
+def test_list_activities(
+    client: FlaskClient,
+    org_id: str,
+    election_id: str,
+    jurisdiction_ids: List[str],
+    audit_board_round_1_ids: List[str],  # pylint: disable=unused-argument
+):
+    set_logged_in_user(client, UserType.AUDIT_ADMIN, DEFAULT_AA_EMAIL)
+    rv = client.get(f"/api/organizations/{org_id}/activities")
+    activities = json.loads(rv.data)
+
+    compare_json(
+        activities,
+        [
+            {
+                "activityName": "CreateAuditBoards",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {
+                    "jurisdiction_id": jurisdiction_ids[0],
+                    "jurisdiction_name": "J1",
+                    "num_audit_boards": 2,
+                },
+                "timestamp": assert_is_date,
+                "user": {
+                    "key": default_ja_email(election_id),
+                    "supportUser": False,
+                    "type": "jurisdiction_admin",
+                },
+            },
+            {
+                "activityName": "StartRound",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {"round_num": 1},
+                "timestamp": assert_is_date,
+                "user": {
+                    "key": DEFAULT_AA_EMAIL,
+                    "supportUser": False,
+                    "type": "audit_admin",
+                },
+            },
+            {
+                "activityName": "CalculateSampleSizes",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {},
+                "timestamp": assert_is_date,
+                "user": {
+                    "key": DEFAULT_AA_EMAIL,
+                    "supportUser": False,
+                    "type": "audit_admin",
+                },
+            },
+            {
+                "activityName": "UploadFile",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {
+                    "error": None,
+                    "file_type": "ballot_manifest",
+                    "jurisdiction_id": jurisdiction_ids[1],
+                    "jurisdiction_name": "J2",
+                },
+                "timestamp": assert_is_date,
+                "user": None,
+            },
+            {
+                "activityName": "UploadFile",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {
+                    "error": None,
+                    "file_type": "ballot_manifest",
+                    "jurisdiction_id": jurisdiction_ids[0],
+                    "jurisdiction_name": "J1",
+                },
+                "timestamp": assert_is_date,
+                "user": None,
+            },
+            {
+                "activityName": "CreateAudit",
+                "election": {
+                    "auditName": "Test Audit test_list_activities",
+                    "auditType": "BALLOT_POLLING",
+                    "id": election_id,
+                },
+                "id": assert_is_id,
+                "info": {},
+                "timestamp": assert_is_date,
+                "user": {
+                    "key": DEFAULT_AA_EMAIL,
+                    "supportUser": False,
+                    "type": "audit_admin",
+                },
+            },
+        ],
+    )
+
+    timestamps = [
+        datetime.fromisoformat(activity["timestamp"]) for activity in activities
+    ]
+    assert timestamps == list(reversed(sorted(timestamps)))
+
+
+def test_list_activities_wrong_org(
+    client: FlaskClient, org_id: str,
+):
+    create_org_and_admin("Test Activities Wrong Org", "other-admin@example.com")
+    set_logged_in_user(client, UserType.AUDIT_ADMIN, "other-admin@example.com")
+    rv = client.get(f"/api/organizations/{org_id}/activities")
+    assert rv.status_code == 403
+
+
+def test_list_activities_wrong_user_type(
+    client: FlaskClient, org_id: str, election_id: str
+):
+    set_logged_in_user(
+        client, UserType.JURISDICTION_ADMIN, default_ja_email(election_id)
+    )
+    rv = client.get(f"/api/organizations/{org_id}/activities")
+    assert rv.status_code == 403
+
+
+def test_list_activities_not_logged_in(client: FlaskClient, org_id: str):
+    clear_logged_in_user(client)
+    rv = client.get(f"/api/organizations/{org_id}/activities")
+    assert rv.status_code == 403
