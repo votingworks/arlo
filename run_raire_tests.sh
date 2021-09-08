2ae3f32f8b0e3b63be68f3fe32ead498e6a635c5
michelleblom <michelle.blom@gmail.com>
2021-09-08 19:25:16 +1000

    Added further tests for raire assertion generation

--- run_raire_tests.sh
+++ run_raire_tests.sh
@@ -1,5 +1,12 @@
 
 for instance in RaireData/Input/*.raire; do
     bn=`basename ${instance}`
+    echo "Running test on instance $bn"
     python3.8 test_raire.py ${instance} RaireData/Output/${bn}.out 
+done
+
+for instance in RaireData/Input/NSW2015/*.raire; do
+    bn=`basename ${instance}`
+    echo "Running test on instance $bn"
+    python3.8 test_raire.py ${instance} RaireData/Output/NSW2015_Output/${bn}.out 
 done
