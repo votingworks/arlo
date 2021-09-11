0e3c81cbaf92214419be5a2fb56c2ed7cafba7f1
michelleblom <michelle.blom@gmail.com>
2021-09-11 16:59:52 +1000

    Added special test case that requires a non zero 'agap'

--- run_raire_tests.sh
+++ run_raire_tests.sh
@@ -2,11 +2,13 @@
 for instance in RaireData/Input/*.raire; do
     bn=`basename ${instance}`
     echo "Running test on instance $bn"
-    python3.8 test_raire.py ${instance} RaireData/Output/${bn}.out 
+    python3.8 test_raire.py -i ${instance} -o RaireData/Output/${bn}.out 
 done
 
 for instance in RaireData/Input/NSW2015/*.raire; do
     bn=`basename ${instance}`
     echo "Running test on instance $bn"
-    python3.8 test_raire.py ${instance} RaireData/Output/NSW2015_Output/${bn}.out 
+    python3.8 test_raire.py -i ${instance} -o RaireData/Output/NSW2015_Output/${bn}.out 
 done
+
+python3.8 test_raire.py -i RaireData/Input/SpecialCases/SanFran_2007.raire -o RaireData/Output/SpecialCases/SanFran_2007.raire.out -agap 0.00001
