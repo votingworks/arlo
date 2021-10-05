95a736cc17eee33cbedc7bd6c08a990ee7c1d837
Jonah Kagan <jonah@voting.works>
2021-10-05 11:04:12 -0700

    Run local nOAuth server with Cypress tests

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ARLO_AUDITADMIN_AUTH0_BASE_URL=https://votingworks-noauth.herokuapp.com
+export ARLO_AUDITADMIN_AUTH0_BASE_URL=http://localhost:8080
 export ARLO_SMTP_HOST=localhost
 export ARLO_SMTP_PORT=1025
 export ARLO_SMTP_USERNAME=cypress-smtp-username
@@ -8,5 +8,6 @@ export ARLO_SMTP_PASSWORD=cypress-smtp-password
 
 trap 'kill 0' SIGINT SIGHUP EXIT
 cd "$(dirname "${BASH_SOURCE[0]}")"
+PORT=8080 poetry run python -m noauth &
 FLASK_ENV=test ../run-dev.sh &
 yarn run cypress run --browser chrome
