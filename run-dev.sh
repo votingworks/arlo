89cfe5ad6a226bd3dd37ff911ea3e1dcb2c8cdc8
Jonah Kagan <jonah@voting.works>
2021-10-05 12:10:03 -0700

    Run local nOAuth server (#1294)

--- run-dev.sh
+++ run-dev.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+export ARLO_AUDITADMIN_AUTH0_BASE_URL=${ARLO_AUDITADMIN_AUTHO_BASE_URL:-http://localhost:8080}
+export ARLO_SUPPORT_AUTH0_BASE_URL=${ARLO_SUPPORT_AUTHO_BASE_URL:-http://localhost:8080}
 export FLASK_ENV=${FLASK_ENV:-development}
 trap 'kill 0' SIGINT SIGHUP
 cd "$(dirname "${BASH_SOURCE[0]}")"
+PORT=8080 poetry run python -m noauth &
 poetry run python -m server.main &
 poetry run python -m server.worker.worker &
 yarn --cwd client start
