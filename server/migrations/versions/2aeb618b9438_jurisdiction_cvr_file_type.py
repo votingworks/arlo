dc0bfadf99d5e261e47b6eea52047a1256623202
Jonah Kagan <jonah@voting.works>
2021-10-06 10:40:21 -0700

    migration

---
+++ server/migrations/versions/2aeb618b9438_jurisdiction_cvr_file_type.py
@@ -0,0 +1,32 @@
+# pylint: disable=invalid-name
+"""Jurisdiction.cvr_file_type
+
+Revision ID: 2aeb618b9438
+Revises: 971d6d153879
+Create Date: 2021-10-06 16:32:19.878853+00:00
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "2aeb618b9438"
+down_revision = "971d6d153879"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "jurisdiction",
+        sa.Column(
+            "cvr_file_type",
+            sa.Enum("DOMINION", "CLEARBALLOT", name="cvrfiletype"),
+            nullable=True,
+        ),
+    )
+
+
+def downgrade():  # pragma: no cover
+    pass
