3003dc46e06e2dae496ccac32574955d62fa9371
Matt Bernhard <mdb92nc@gmail.com>
2021-12-06 22:41:11 +0000

    First pass at RAIRE supersimple implementation

---
+++ server/audit_math/supersimple_raire.py
@@ -0,0 +1,212 @@
+# pylint: disable=invalid-name
+from itertools import product
+from decimal import Decimal
+from typing import Dict, Tuple, Optional, List
+
+from .sampler_contest import Contest, CVRS, SAMPLECVRS, CVR
+
+from .supersimple import Discrepancy
+from .raire_utils import RaireAssertion
+
+l: Decimal = Decimal(0.5)
+gamma: Decimal = Decimal(1.03905)  # This gamma is used in Stark's tool, AGI, and CORLA
+
+# This sets the expected number of one-vote misstatements at 1 in 1000
+o1: Decimal = Decimal(0.001)
+u1: Decimal = Decimal(0.001)
+
+# This sets the expected two-vote misstatements at 1 in 10000
+o2: Decimal = Decimal(0.0001)
+u2: Decimal = Decimal(0.0001)
+
+
+def compute_discrepancies(
+    contest: Contest, cvrs: CVRS, sample_cvr: SAMPLECVRS, assertion: RaireAssertion
+) -> Dict[str, Discrepancy]:
+    """
+    Iterates through a given sample and returns the discrepancies found.
+
+    Inputs:
+        contests       - the contests and results being audited
+        cvrs           - mapping of ballot_id to votes:
+                {
+                    'ballot_id': {
+                        'contest': {
+                            'candidate1': 1,
+                            'candidate2': 2,
+                            ...
+                        }
+                    ...
+                }
+
+        sample_cvr - the CVR of the audited ballots
+                {
+                    'ballot_id': {
+                        'times_sampled': 1,
+                        'cvr': {
+                            'contest': {
+                                'candidate1': 1,
+                                'candidate2': 2,
+                                ...
+                            }
+                    }
+                    ...
+                }
+
+    Outputs:
+        discrepancies   - A mapping of ballot ids to their discrepancies. This
+                          includes entries for ballots in the sample that have discrepancies
+                          only.
+                    {
+                        'ballot_id': {
+                            'counted_as': -1, # The maximum value for a discrepancy
+                            'weighted_error': -0.0033 # Weighted error used for p-value calculation
+                        }
+                    }
+    """
+
+    discrepancies: Dict[str, Discrepancy] = {}
+    for ballot, ballot_sample_cvr in sample_cvr.items():
+        ballot_discrepancies = []
+        for winner, loser in product(contest.winners, contest.losers):
+            ballot_discrepancy = discrepancy(
+                contest,
+                winner,
+                loser,
+                cvrs.get(ballot),
+                ballot_sample_cvr["cvr"],
+                assertion,
+            )
+            if ballot_discrepancy is not None:
+                ballot_discrepancies.append(ballot_discrepancy)
+
+        if len(ballot_discrepancies) > 0:
+            discrepancies[ballot] = max(
+                ballot_discrepancies, key=lambda d: d["counted_as"]
+            )
+
+    return discrepancies
+
+
+def discrepancy(
+    contest: Contest,
+    winner: str,
+    loser: str,
+    reported: Optional[CVR],
+    audited: Optional[CVR],
+    assertion: RaireAssertion,
+) -> Optional[Discrepancy]:
+    # Special cases: if ballot wasn't in CVR or ballot can't be found by
+    # audit board, count it as a two-vote overstatement
+    if reported is None or audited is None:
+        error = 2
+    else:
+        v_w, v_l = (
+            assertion.is_vote_for_winner(reported),
+            assertion.is_vote_for_loser(reported),
+        )
+        a_w, a_l = (
+            assertion.is_vote_for_winner(audited),
+            assertion.is_vote_for_loser(audited),
+        )
+        error = (v_w - a_w) - (v_l - a_l)
+
+    if error == 0:
+        return None
+
+    # V_wl here is defined as the first-round tallies for winner and loser
+    V_wl = contest.candidates[winner] - contest.candidates[loser]
+    weighted_error = Decimal(error) / Decimal(V_wl) if V_wl > 0 else Decimal("inf")
+
+    return Discrepancy(counted_as=error, weighted_error=weighted_error)
+
+
+def compute_risk(
+    risk_limit: int,
+    contest: Contest,
+    cvrs: CVRS,
+    sample_cvr: SAMPLECVRS,
+    assertions: List[RaireAssertion],
+) -> Tuple[float, bool]:
+    """
+    Computes the risk-value of <sample_results> based on results in <contest>.
+
+    Inputs:
+        contests       - the contests and results being audited
+        cvrs           - mapping of ballot_id to votes:
+                {
+                    'ballot_id': {
+                        'contest': {
+                            'candidate1': 1,
+                            'candidate2': 0,
+                            ...
+                        }
+                    ...
+                }
+
+        sample_cvr - the CVR of the audited ballots
+                {
+                    'ballot_id': {
+                        'times_sampled': 1,
+                        'contest': {
+                            'candidate1': 1,
+                            'candidate2': 0,
+                            ...
+                        }
+                    ...
+                }
+
+    Outputs:
+        measurements    - the p-value of the hypotheses that the election
+                          result is correct based on the sample, for each winner-loser pair.
+        confirmed       - a boolean indicating whether the audit can stop
+    """
+    alpha = Decimal(risk_limit) / 100
+    assert alpha < 1
+
+    N = contest.ballots
+    V = Decimal(contest.diluted_margin * N)
+    max_p = Decimal(0.0)
+    result = False
+    for assertion in assertions:
+        p = Decimal(1.0)
+
+        if contest.diluted_margin == 0:
+            U = Decimal("inf")
+        else:
+            U = 2 * gamma / Decimal(contest.diluted_margin)
+
+        discrepancies = compute_discrepancies(contest, cvrs, sample_cvr, assertion)
+
+        for ballot in sample_cvr:
+            if ballot in discrepancies:
+                # We want to be conservative, so we will ignore understatements (i.e. errors
+                # that favor the winner) which are negative.
+                e_r = max(discrepancies[ballot]["weighted_error"], Decimal(0))
+            else:
+                e_r = Decimal(0)
+
+            if contest.diluted_margin:
+                U = 2 * gamma / Decimal(contest.diluted_margin)
+                denom = (2 * gamma) / V
+                p_b = (1 - 1 / U) / (1 - (e_r / denom))
+            else:
+                # If the contest is a tie, this step results in 1 - 1/(infinity)
+                # divided by 1 - e_r/infinity, i.e. 1
+                p_b = Decimal(1.0)
+
+            multiplicity = sample_cvr[ballot]["times_sampled"]
+            p *= p_b ** multiplicity
+
+        # Get the largest p-value across all assertions
+        if p > max_p:
+            max_p = p
+
+    if 0 < max_p < alpha:
+        result = True
+
+    # Special case if the sample size equals all the ballots (i.e. a full hand tally)
+    if sum(ballot["times_sampled"] for ballot in sample_cvr.values()) >= N:
+        return 0, True
+
+    return min(float(max_p), 1.0), result
---
+++ server/tests/audit_math/test_supersimple_raire.py
@@ -0,0 +1,493 @@
+# pylint: disable=invalid-name
+from decimal import Decimal
+import pytest
+import numpy as np
+
+from ...audit_math import supersimple
+from ...audit_math.sampler_contest import Contest
+from ...audit_math.raire import compute_raire_assertions
+
+from .test_raire_utils import make_neb_assertion
+from ...audit_math import supersimple_raire
+
+seed = "12345678901234567890abcdefghijklmnopqrstuvwxyz😊"
+ALPHA = Decimal(0.1)
+RISK_LIMIT = 10
+
+# Testing for now...
+asn_func = lambda m: 1 / m if m > 0 else np.inf
+
+
+@pytest.fixture
+def cvrs():
+    cvr = {}
+    for i in range(100000):
+        if i < 60000:
+            contest_a_res = {"winner": 1, "loser": 2}
+        else:
+            contest_a_res = {"winner": 2, "loser": 1}
+
+        cvr[i] = {"Contest A": contest_a_res}
+
+        if i < 30000:
+            cvr[i]["Contest B"] = {"winner": 1, "loser": 2}
+        elif 30000 < i < 60000:
+            cvr[i]["Contest B"] = {"winner": 2, "loser": 1}
+
+        if i < 18000:
+            cvr[i]["Contest C"] = {"winner": 1, "loser": 2}
+        elif 18000 < i < 36000:
+            cvr[i]["Contest C"] = {"winner": 2, "loser": 1}
+
+        if i < 8000:
+            cvr[i]["Contest D"] = {"winner": 1, "loser": 2}
+        elif 8000 < i < 14000:
+            cvr[i]["Contest D"] = {"winner": 2, "loser": 1}
+
+        if i < 10000:
+            cvr[i]["Contest E"] = {"winner": 1, "loser": 2}
+
+    yield cvr
+
+
+@pytest.fixture
+def contests():
+    contests = {}
+
+    for contest in ss_contests:
+        contests[contest] = Contest(contest, ss_contests[contest])
+
+    yield contests
+
+
+@pytest.fixture()
+def assertions(contests, cvrs):
+    assertions = {}
+    for contest in contests:
+        assertions[contest] = make_neb_assertion(
+            contests[contest], cvrs, asn_func, "winner", "loser", []
+        )
+    return assertions
+
+
+def test_find_no_discrepancies(contests, cvrs, assertions):
+
+    # Test no discrepancies
+    sample_cvr = {
+        0: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {"winner": 1, "loser": 2},
+                "Contest B": {"winner": 1, "loser": 2},
+                "Contest C": {"winner": 1, "loser": 2},
+                "Contest D": {"winner": 1, "loser": 2},
+                "Contest E": {"winner": 1, "loser": 2},
+            },
+        }
+    }
+
+    for contest in contests:
+        discrepancies = supersimple_raire.compute_discrepancies(
+            contests[contest], cvrs, sample_cvr, assertions[contest]
+        )
+        assert not discrepancies
+
+
+def test_find_one_discrepancy(contests, cvrs, assertions):
+
+    # Test one discrepancy
+    sample_cvr = {
+        0: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {"winner": 0, "loser": 0},
+                "Contest B": {"winner": 1, "loser": 2},
+                "Contest C": {"winner": 1, "loser": 2},
+                "Contest D": {"winner": 1, "loser": 2},
+                "Contest E": {"winner": 1, "loser": 2},
+            },
+        }
+    }
+
+    for contest in contests:
+        discrepancies = supersimple_raire.compute_discrepancies(
+            contests[contest], cvrs, sample_cvr, assertions[contest]
+        )
+
+        if contest == "Contest A":
+            assert discrepancies[0]["counted_as"] == 1
+            assert discrepancies[0]["weighted_error"] == Decimal(1) / Decimal(20000)
+        else:
+            assert not discrepancies
+
+
+def test_negative_discrepancies(contests, cvrs, assertions):
+    sample_cvr = {
+        60000: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {
+                    "winner": 1,
+                    "loser": 2,
+                },  # One of the reported loser ballots was actually a winner ballot
+            },
+        }
+    }
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest A"], cvrs, sample_cvr, assertions["Contest A"]
+    )
+
+    assert discrepancies
+    assert discrepancies[60000]["counted_as"] == -2
+    assert discrepancies[60000]["weighted_error"] == Decimal(-2) / Decimal(20000)
+
+
+def test_two_vote_overstatement_discrepancies(contests, cvrs, assertions):
+    sample_cvr = {
+        0: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {
+                    "winner": 2,
+                    "loser": 1,
+                },  # One of the reported winner ballots was actually a loser ballot
+            },
+        }
+    }
+
+    contest = "Contest A"
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests[contest], cvrs, sample_cvr, assertions[contest]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 2
+    assert discrepancies[0]["weighted_error"] == Decimal(2) / Decimal(20000)
+
+
+def test_race_not_in_cvr_discrepancy(contests, cvrs, assertions):
+
+    sample_cvr = {
+        0: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest F": {
+                    "winner": 0,
+                    "loser": 1,
+                },  # The audit board found a race not in the CVR
+            },
+        }
+    }
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest F"], cvrs, sample_cvr, assertions["Contest F"]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 1
+    assert discrepancies[0]["weighted_error"] == Decimal(1) / Decimal(6)
+
+
+def test_race_not_in_sample_discrepancy(contests, cvrs, assertions):
+
+    sample_cvr = {
+        0: {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {"winner": 0, "loser": 0},
+                "Contest B": {"winner": 1, "loser": 2},
+                "Contest C": {"winner": 1, "loser": 2},
+                "Contest E": {"winner": 1, "loser": 2},
+            },
+        }
+    }
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest D"], cvrs, sample_cvr, assertions["Contest D"]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 1
+    assert discrepancies[0]["weighted_error"] == Decimal(1) / Decimal(2000)
+
+
+def test_ballot_not_found_discrepancy(contests, cvrs, assertions):
+    sample_cvr = {0: {"times_sampled": 1, "cvr": None}}
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest D"], cvrs, sample_cvr, assertions["Contest D"]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 2
+    assert discrepancies[0]["weighted_error"] == Decimal(2) / Decimal(2000)
+
+
+def test_ballot_not_in_cvr(contests, assertions):
+    cvrs = {}
+    sample_cvr = {
+        0: {"times_sampled": 1, "cvr": {"Contest D": {"winner": 1, "loser": 2}}}
+    }
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest D"], cvrs, sample_cvr, assertions["Contest D"]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 2
+    assert discrepancies[0]["weighted_error"] == Decimal(2) / Decimal(2000)
+
+
+def test_ballot_not_in_cvr_and_not_found(contests, assertions):
+    cvrs = {}
+    sample_cvr = {0: {"times_sampled": 1, "cvr": None}}
+
+    discrepancies = supersimple_raire.compute_discrepancies(
+        contests["Contest D"], cvrs, sample_cvr, assertions["Contest D"]
+    )
+
+    assert discrepancies
+    assert discrepancies[0]["counted_as"] == 2
+    assert discrepancies[0]["weighted_error"] == Decimal(2) / Decimal(2000)
+
+
+def test_fptp(contests, cvrs, assertions):
+    # RAIRE should give us identical results to a first-past-the-post election if
+    # there are only two candidates
+
+    for contest in contests:
+
+        computed_assertions = compute_raire_assertions(
+            contests[contest], cvrs, "winner", asn_func, 0
+        )
+
+        if contest == "Contest F":
+            assert not computed_assertions
+        else:
+            expected_assertions = [assertions[contest]]
+            assert computed_assertions == expected_assertions, f"Failed for {contest}"
+
+        sample_cvr = {}
+        sample_size = supersimple.get_sample_sizes(RISK_LIMIT, contests[contest], None)
+
+        # No discrepancies
+        for i in range(sample_size):
+            sample_cvr[i] = {
+                "times_sampled": 1,
+                "cvr": {
+                    "Contest A": {"winner": 1, "loser": 2},
+                    "Contest B": {"winner": 1, "loser": 2},
+                    "Contest C": {"winner": 1, "loser": 2},
+                    "Contest D": {"winner": 1, "loser": 2},
+                    "Contest E": {"winner": 1, "loser": 2},
+                },
+            }
+
+        p_value, finished = supersimple_raire.compute_risk(
+            RISK_LIMIT, contests[contest], cvrs, sample_cvr, computed_assertions
+        )
+
+        expected_p = expected_p_values["no_discrepancies"][contest]
+        diff = abs(p_value - expected_p)
+
+        assert (
+            diff < 0.001
+        ), "Incorrect p-value. Expected {}, got {} in contest {}".format(
+            expected_p, p_value, contest
+        )
+        if contest != "Contest F":
+            assert finished, f"Audit of {contest} should have finished but didn't"
+
+        to_sample = {
+            "sample_size": sample_size,
+            "1-under": 0,
+            "1-over": 0,
+            "2-under": 0,
+            "2-over": 0,
+        }
+
+        next_sample_size = supersimple.get_sample_sizes(
+            RISK_LIMIT, contests[contest], to_sample
+        )
+        assert next_sample_size == 0
+
+        # Test one-vote overstatement
+        sample_cvr[0] = {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {"winner": 0, "loser": 0},
+                "Contest B": {"winner": 0, "loser": 0},
+                "Contest C": {"winner": 0, "loser": 0},
+                "Contest D": {"winner": 0, "loser": 0},
+                "Contest E": {"winner": 0, "loser": 0},
+            },
+        }
+
+        p_value, finished = supersimple_raire.compute_risk(
+            RISK_LIMIT, contests[contest], cvrs, sample_cvr, computed_assertions
+        )
+
+        expected_p = expected_p_values["one_vote_over"][contest]
+        diff = abs(p_value - expected_p)
+
+        assert (
+            diff < 0.001
+        ), "Incorrect p-value. Expected {}, got {} in contest {}".format(
+            expected_p, p_value, contest
+        )
+        if contest in ["Contest E"]:
+            assert finished, "Audit should have finished but didn't"
+        else:
+            assert not finished, "Audit shouldn't have finished but did!"
+
+        to_sample = {
+            "sample_size": sample_size,
+            "1-under": 0,
+            "1-over": 1,
+            "2-under": 0,
+            "2-over": 0,
+        }
+
+        next_sample_size = supersimple.get_sample_sizes(
+            RISK_LIMIT, contests[contest], to_sample
+        )
+        assert (
+            next_sample_size == o1_stopping_size[contest] - sample_size
+        ), "Number of ballots left to sample is not correct in contest {}!".format(
+            contest
+        )
+
+        # Test two-vote overstatement
+        sample_cvr[0] = {
+            "times_sampled": 1,
+            "cvr": {
+                "Contest A": {"winner": 0, "loser": 1},
+                "Contest B": {"winner": 0, "loser": 1},
+                "Contest C": {"winner": 0, "loser": 1},
+                "Contest D": {"winner": 0, "loser": 1},
+                "Contest E": {"winner": 0, "loser": 1},
+            },
+        }
+
+        p_value, finished = supersimple_raire.compute_risk(
+            RISK_LIMIT, contests[contest], cvrs, sample_cvr, computed_assertions
+        )
+        expected_p = expected_p_values["two_vote_over"][contest]
+        diff = abs(p_value - expected_p)
+
+        assert (
+            diff < 0.001
+        ), "Incorrect p-value. Expected {}, got {} in contest {}".format(
+            expected_p, p_value, contest
+        )
+
+        assert not finished, "Audit shouldn't have finished but did!"
+
+        to_sample = {
+            "sample_size": sample_size,
+            "1-under": 0,
+            "1-over": 0,
+            "2-under": 0,
+            "2-over": 1,
+        }
+
+        next_sample_size = supersimple.get_sample_sizes(
+            RISK_LIMIT, contests[contest], to_sample
+        )
+        assert (
+            next_sample_size == o2_stopping_size[contest] - sample_size
+        ), "Number of ballots left to sample is not correct in contest {}!".format(
+            contest
+        )
+
+
+ss_contests = {
+    "Contest A": {
+        "winner": 60000,
+        "loser": 40000,
+        "ballots": 100000,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+    "Contest B": {
+        "winner": 30000,
+        "loser": 24000,
+        "ballots": 60000,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+    "Contest C": {
+        "winner": 18000,
+        "loser": 12600,
+        "ballots": 36000,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+    "Contest D": {
+        "winner": 8000,
+        "loser": 6000,
+        "ballots": 15000,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+    "Contest E": {
+        "winner": 10000,
+        "loser": 0,
+        "ballots": 10000,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+    "Contest F": {
+        "winner": 10,
+        "loser": 4,
+        "ballots": 15,
+        "numWinners": 1,
+        "votesAllowed": 1,
+    },
+}
+
+expected_p_values = {
+    "no_discrepancies": {
+        "Contest A": 0.06507,
+        "Contest B": 0.06973,
+        "Contest C": 0.06740,
+        "Contest D": 0.07048,
+        "Contest E": 0.01950,
+        "Contest F": 0.0,  # Full recount
+    },
+    "one_vote_over": {
+        "Contest A": 0.12534,
+        "Contest B": 0.13441,
+        "Contest C": 0.12992,
+        "Contest D": 0.13585,
+        "Contest E": 0.03758,
+        "Contest F": 0.0,  # Full recount
+    },
+    "two_vote_over": {
+        "Contest A": 1.0,
+        "Contest B": 1.0,
+        "Contest C": 1.0,
+        "Contest D": 1.0,
+        "Contest E": 0.51877,
+        "Contest F": 0.0,  # Full recount
+    },
+}
+
+o1_stopping_size = {
+    "Contest A": 38,
+    "Contest B": 76,
+    "Contest C": 51,
+    "Contest D": 57,
+    "Contest E": 7,
+    "Contest F": 15,  # nMin yields 16, but contest only has 15 total votes
+}
+
+o2_stopping_size = {
+    "Contest A": 100000,
+    "Contest B": 60000,
+    "Contest C": 36000,
+    "Contest D": 15000,
+    "Contest E": 6,
+    "Contest F": 15,
+}
