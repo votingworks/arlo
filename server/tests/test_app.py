c04ae532cf9f0a1be067536b37f4b2b49a407a1e
Jonah Kagan <jonah@voting.works>
2022-03-01 14:38:37 -0800

    Fix test

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -3,13 +3,13 @@ from flask.testing import FlaskClient
 
 def test_index(client: FlaskClient):
     rv = client.get("/")
-    assert b"Arlo (by VotingWorks)" in rv.data
+    assert b"Arlo, by VotingWorks" in rv.data
 
     rv = client.get("/election/1234")
-    assert b"Arlo (by VotingWorks)" in rv.data
+    assert b"Arlo, by VotingWorks" in rv.data
 
     rv = client.get("/election/1234/audit-board/5677")
-    assert b"Arlo (by VotingWorks)" in rv.data
+    assert b"Arlo, by VotingWorks" in rv.data
 
 
 def test_static_logo(client: FlaskClient):
