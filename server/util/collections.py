291f2d1e4e7f34b572ff24082bc0e92873c34534
Jonah Kagan <jonah@voting.works>
2022-05-04 08:49:28 -0700

    add comments

--- server/util/collections.py
+++ server/util/collections.py
@@ -9,10 +9,12 @@ def group_by_iter(items: Iterable[T], key=None):
     return itertools.groupby(sorted(items, key=key), key=key)
 
 
+# group_by groups items in a collection according to a key function.
 def group_by(items: Iterable[T], key=None):
     return {k: list(vs) for k, vs in group_by_iter(items, key=key)}
 
 
+# find_first_duplicate returns the first item in a collection that is a duplicate.
 def find_first_duplicate(list: Iterable[T]) -> Optional[T]:
     seen = set()
     for item in list:
