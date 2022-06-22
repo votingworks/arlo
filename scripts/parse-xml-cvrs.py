ae558fa8289ab78346ce48b99d7e296d132ef333
Jonah Kagan <jonah@voting.works>
2022-06-21 19:02:18 -0500

    Modify parse-xml-cvrs script to convert Hart CVRs to Dominion CVRs (#1288)
    * Convert Hart CVRs all the way to Dominion format
    
    * Augment Hart-CVR-parsing script to support Orange County's Hart CVRs
    
    Co-authored-by: Arsalan Sufi <arsalan@voting.works>

--- scripts/parse-xml-cvrs.py
+++ scripts/parse-xml-cvrs.py
@@ -18,7 +18,13 @@ def findall(xml, tag):
     return xml.findall(tag, namespaces={"": ns})
 
 
-def parse_cvr_file(file_path):
+def get_directory_name(file_path):
+    directory_path = os.path.dirname(file_path)
+    directory_name = os.path.basename(directory_path)
+    return directory_name
+
+
+def parse_cvr_file(file_path, use_directory_name_as_tabulator=False):
     xml = ElementTree.parse(file_path).getroot()
     assert xml.tag == f"{{{ns}}}Cvr"
 
@@ -43,35 +49,57 @@ def parse_cvr_file(file_path):
             vote = find(choice, "Value").text
             cvr["Contests"][contest_name][choice_name] = vote
 
+    if use_directory_name_as_tabulator:
+        cvr["Tabulator"] = get_directory_name(file_path)
+
     return cvr
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if not (
+        len(sys.argv) == 3
+        or (len(sys.argv) == 4 and sys.argv[1] == "--cvrs-exported-by-tabulator")
+    ):
         print(
-            "Usage: python -m scripts.parse-xml-cvrs <cvr_directory_path> <output_csv_path>",
+            "Usage: python -m scripts.parse-xml-cvrs [--cvrs-exported-by-tabulator] <cvr_directory_path> <output_csv_path>",
             file=sys.stderr,
         )
         sys.exit(1)
 
-    cvr_directory_path = sys.argv[1]
-    output_csv_path = sys.argv[2]
+    cvr_directory_path = sys.argv[len(sys.argv) - 2]
+    output_csv_path = sys.argv[len(sys.argv) - 1]
+    cvrs_exported_by_tabulator = len(sys.argv) == 4
 
-    contest_choices: dict = defaultdict(set)
+    print("Finding CVR files...")
+
+    cvr_file_paths = []
+    if cvrs_exported_by_tabulator:
+        for entry in os.scandir(cvr_directory_path):
+            if entry.is_dir():
+                for sub_entry in os.scandir(entry.path):
+                    if sub_entry.is_file() and sub_entry.name.endswith(".xml"):
+                        cvr_file_paths.append(sub_entry.path)
+    else:
+        for entry in os.scandir(cvr_directory_path):
+            if entry.is_file() and entry.name.endswith(".xml"):
+                cvr_file_paths.append(entry.path)
+
+    print(f"Found {len(cvr_file_paths)} CVR files")
+
     cvrs = []
-
-    for entry in os.scandir(cvr_directory_path):
-        if entry.is_dir():
-            print(f"Skipping directory: {entry.name}")
-            continue
-
+    contest_choices: dict = defaultdict(set)
+    for cvr_file_path in cvr_file_paths:
         try:
-            cvr = parse_cvr_file(entry.path)
+            cvr = parse_cvr_file(
+                cvr_file_path,
+                use_directory_name_as_tabulator=cvrs_exported_by_tabulator,
+            )
         except Exception as exc:
-            print(f"Error parsing file: {entry.path}")
+            print(f"Error parsing file: {cvr_file_path}")
             raise exc
 
         cvrs.append(cvr)
+
         # Keep track of all contest choices we've seen
         for contest_name, choices in cvr["Contests"].items():
             for choice_name in choices:
@@ -82,41 +110,52 @@ if __name__ == "__main__":
 
     print("Writing CSV...")
 
+    contest_choice_pairs = [
+        (contest_name, choice_name)
+        for contest_name, choices in contest_choices.items()
+        for choice_name in choices
+    ]
+
     with open(output_csv_path, "w") as output_file:
-        headers = [
-            "CvrGuid",
-            "BatchNumber",
-            "BatchSequence",
-            "SheetNumber",
-            "PrecinctSplit",
-        ] + [
-            f"{contest_name};{choice_name}"
-            for contest_name, choices in contest_choices.items()
-            for choice_name in choices
+
+        writer = csv.writer(output_file)
+        writer.writerow(["Election Name", "0.00.0.00"])
+
+        contest_headers = ["", "", "", "", ""] + [
+            f"{contest_name} (Vote For=1)" for contest_name, _ in contest_choice_pairs
         ]
+        writer.writerow(contest_headers)
 
-        writer = csv.DictWriter(output_file, fieldnames=headers)
-        writer.writeheader()
+        choice_headers = ["", "", "", "", ""] + [
+            choice_name for _, choice_name in contest_choice_pairs
+        ]
+        writer.writerow(choice_headers)
 
-        # Flatten contest choice votes into field names
-        # Fill in missing contest choices with 0s
-        for cvr in cvrs:
-            flat_cvr = {
-                "CvrGuid": cvr["CvrGuid"],
-                "BatchNumber": cvr["BatchNumber"],
-                "BatchSequence": cvr["BatchSequence"],
-                "SheetNumber": cvr["SheetNumber"],
-                "PrecinctSplit": cvr["PrecinctSplit"],
-            }
+        headers = [
+            "CvrNumber",
+            "TabulatorNum",
+            "BatchId",
+            "RecordId",
+            "ImprintedId",
+        ] + ["NP" for _ in contest_choice_pairs]
+        writer.writerow(headers)
 
-            for contest_name, choices in contest_choices.items():
-                for choice_name in choices:
-                    if contest_name not in cvr["Contests"]:
-                        vote = None
-                    elif choice_name not in cvr["Contests"][contest_name]:
-                        vote = 0
-                    else:
-                        vote = cvr["Contests"][contest_name][choice_name]
-                    flat_cvr[f"{contest_name};{choice_name}"] = vote
+        for i, cvr in enumerate(cvrs):
+            row = [
+                i,
+                cvr["Tabulator"] if cvrs_exported_by_tabulator else 1,
+                cvr["BatchNumber"],
+                cvr["BatchSequence"],
+                cvr["CvrGuid"],
+            ]
 
-            writer.writerow(flat_cvr)
+            # Fill in missing contest choices with 0s
+            for contest_name, choice_name in contest_choice_pairs:
+                if contest_name not in cvr["Contests"]:
+                    row.append("")
+                elif choice_name not in cvr["Contests"][contest_name]:
+                    row.append(0)
+                else:
+                    row.append(cvr["Contests"][contest_name][choice_name])
+
+            writer.writerow(row)
