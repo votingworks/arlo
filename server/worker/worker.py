a92e3004b29aa34b0c5b38ca637120047e5e69ac
Jonah Kagan <jonah@voting.works>
2022-06-29 10:05:54 -0700

    Don't crash worker on caught task errors
    Any error that occurs during a task that we can catch and save to the
    database, we should send to Sentry but not crash the worker, since it's
    safe to continue working. Any error that happens in the rest of the
    worker outside of the task context can still crash as before.

--- server/worker/worker.py
+++ server/worker/worker.py
@@ -4,6 +4,10 @@ from server.database import db_session
 from server.worker.tasks import run_new_tasks
 from server.sentry import configure_sentry
 
+# We have to import all of the api so that the modules load and all of the task
+# handlers get registered as background_tasks.
+from server import api  # pylint: disable=unused-import
+
 if __name__ == "__main__":
     configure_sentry()
     while True:
