34770e246afe016ab576bbb02528c02bbb88e7e1
Arsalan Sufi <arsalan@voting.works>
2022-08-26 14:30:24 -0400

    Use StyledTable instead of Table

--- client/src/components/AuditAdmin/Progress/AuditBoardsTable.tsx
+++ client/src/components/AuditAdmin/Progress/AuditBoardsTable.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Button } from '@blueprintjs/core'
-import { Cell, Column } from 'react-table'
 
 import { Confirm, useConfirm } from '../../Atoms/Confirm'
-import { Table } from '../../Atoms/Table'
+import { StyledTable } from '../../Atoms/Table'
 
 interface IAuditBoardMinimal {
   id: string
@@ -19,42 +18,40 @@ interface IProps {
 const AuditBoardsTable = ({ auditBoards, reopenAuditBoard }: IProps) => {
   const { confirm, confirmProps } = useConfirm()
 
-  const columns: Column<IAuditBoardMinimal>[] = [
-    {
-      Header: 'Audit Board',
-      Cell: (cell: Cell<IAuditBoardMinimal>) => {
-        const auditBoard = cell.row.original
-        return auditBoard.name
-      },
-    },
-  ]
-  if (reopenAuditBoard) {
-    columns.push({
-      Header: 'Actions',
-      // eslint-disable-next-line react/display-name
-      Cell: (cell: Cell<IAuditBoardMinimal>) => {
-        const auditBoard = cell.row.original
-        return (
-          <Button
-            disabled={!auditBoard.signedOffAt}
-            onClick={() =>
-              confirm({
-                title: 'Confirm',
-                description: `Are you sure you want to reopen ${auditBoard.name}?`,
-                yesButtonLabel: 'Reopen',
-                onYesClick: () => reopenAuditBoard(auditBoard),
-              })
-            }
-          >
-            Reopen
-          </Button>
-        )
-      },
-    })
-  }
   return (
     <>
-      <Table columns={columns} data={auditBoards} />
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>Audit Board</th>
+            {reopenAuditBoard && <th>Actions</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {auditBoards.map(auditBoard => (
+            <tr key={auditBoard.id}>
+              <td>{auditBoard.name}</td>
+              {reopenAuditBoard && (
+                <td>
+                  <Button
+                    disabled={!auditBoard.signedOffAt}
+                    onClick={() =>
+                      confirm({
+                        title: 'Confirm',
+                        description: `Are you sure you want to reopen ${auditBoard.name}?`,
+                        yesButtonLabel: 'Reopen',
+                        onYesClick: () => reopenAuditBoard(auditBoard),
+                      })
+                    }
+                  >
+                    Reopen
+                  </Button>
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
       <Confirm {...confirmProps} />
     </>
   )
