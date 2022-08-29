5c47b3bf07db3dc28302b2d78df12270a9330f8a
Arsalan Sufi <arsalan@voting.works>
2022-08-29 17:07:42 -0400

    Give support users btns for undoing round starts and reopening rounds

---
+++ client/src/components/SupportTools/RoundsTable.tsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import { Button } from '@blueprintjs/core'
+import { toast } from 'react-toastify'
+
+import { Confirm, useConfirm } from '../Atoms/Confirm'
+import { IRound, useReopenCurrentRound, useUndoRoundStart } from './support-api'
+import { StyledTable } from '../Atoms/Table'
+import StatusTag from '../Atoms/StatusTag'
+
+interface IProps {
+  electionId: string
+  rounds: IRound[]
+}
+
+const RoundsTable = ({ electionId, rounds }: IProps) => {
+  if (rounds.length === 0) {
+    return (
+      <StyledTable style={{ tableLayout: 'auto' }}>
+        <thead>
+          <tr>
+            <th>Round</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Round 1</td>
+            <td>
+              <StatusTag>Not started</StatusTag>
+            </td>
+          </tr>
+        </tbody>
+      </StyledTable>
+    )
+  }
+
+  return (
+    <StyledTable style={{ tableLayout: 'auto' }}>
+      <thead>
+        <tr>
+          <th>Round</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rounds.map((round, i) => (
+          <tr key={round.id}>
+            <td>Round {round.roundNum}</td>
+            <td>
+              {round.endedAt ? (
+                <StatusTag intent="success">Completed</StatusTag>
+              ) : (
+                <StatusTag intent="warning">In progress</StatusTag>
+              )}
+            </td>
+            <td>
+              {i === rounds.length - 1 && (
+                <LastRoundAction electionId={electionId} round={round} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </StyledTable>
+  )
+}
+
+interface ILastRoundActionProps {
+  electionId: string
+  round: IRound
+}
+
+const LastRoundAction = ({ electionId, round }: ILastRoundActionProps) => {
+  const { confirm, confirmProps } = useConfirm()
+  const reopenCurrentRound = useReopenCurrentRound()
+  const undoRoundStart = useUndoRoundStart()
+
+  if (round.endedAt) {
+    return (
+      <>
+        <Button
+          onClick={() =>
+            confirm({
+              title: 'Confirm',
+              description: `Are you sure you want to reopen Round ${round.roundNum}?`,
+              yesButtonLabel: 'Reopen',
+              onYesClick: async () => {
+                await reopenCurrentRound.mutateAsync({ electionId })
+                toast.success(`Reopened Round ${round.roundNum}`)
+              },
+            })
+          }
+        >
+          Reopen
+        </Button>
+        <Confirm {...confirmProps} />
+      </>
+    )
+  }
+  return (
+    <>
+      <Button
+        onClick={() =>
+          confirm({
+            title: 'Confirm',
+            description: `Are you sure you want to undo the start of Round ${round.roundNum}?`,
+            yesButtonLabel: 'Undo Start',
+            onYesClick: async () => {
+              await undoRoundStart.mutateAsync({
+                electionId,
+                roundId: round.id,
+              })
+              toast.success(`Undid the start of Round ${round.roundNum}`)
+            },
+          })
+        }
+      >
+        Undo Start
+      </Button>
+      <Confirm {...confirmProps} />
+    </>
+  )
+}
+
+export default RoundsTable
