a86c60be18d402865504507afdf592c5fb5e2384
Jonah Kagan <jonah@voting.works>
2022-09-08 12:47:20 -0700

    Turn off stylelint property order rule
    It can't autofix so it just wastes time. Nobody cares about your stupid property order, stylelint!

--- client/.stylelintrc.js
+++ client/.stylelintrc.js
@@ -1,15 +1,15 @@
 // Config for stylelint parsing CSS in Styled Components
 // Note the `--fix` flag doesn't yet work for CSS-in-JS
 module.exports = {
-    processors: ['stylelint-processor-styled-components'],
-    extends: [
-      'stylelint-config-palantir',
-      'stylelint-config-prettier',
-      'stylelint-config-styled-components',
-    ],
-    rules: {
-      'selector-max-id': 1,
-      'selector-max-universal': 1,
-    },
-  }
-  
+  processors: ['stylelint-processor-styled-components'],
+  extends: [
+    'stylelint-config-palantir',
+    'stylelint-config-prettier',
+    'stylelint-config-styled-components',
+  ],
+  rules: {
+    'selector-max-id': 1,
+    'selector-max-universal': 1,
+    'order/properties-order': null,
+  },
+}
