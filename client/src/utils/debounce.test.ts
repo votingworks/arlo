da3259e681e2974596979e5373dbe881007dcce2
Arsalan Sufi <arsalan@voting.works>
2022-09-19 12:55:48 -0400

    Add hook for debouncing values

---
+++ client/src/utils/debounce.test.ts
@@ -0,0 +1,27 @@
+import { renderHook } from '@testing-library/react-hooks'
+
+import { useDebounce } from './debounce'
+
+test('useDebounce debounces provided value', async () => {
+  const { rerender, result, waitForNextUpdate } = renderHook(
+    ({ value }) => useDebounce(value),
+    { initialProps: { value: 'value' } }
+  )
+
+  let [debouncedValue, isDebouncing] = result.current
+  expect(debouncedValue).toEqual('value')
+  expect(isDebouncing).toEqual(true)
+  await waitForNextUpdate()
+  ;[debouncedValue, isDebouncing] = result.current
+  expect(debouncedValue).toEqual('value')
+  expect(isDebouncing).toEqual(false)
+
+  rerender({ value: 'newValue' })
+  ;[debouncedValue, isDebouncing] = result.current
+  expect(debouncedValue).toEqual('value')
+  expect(isDebouncing).toEqual(true)
+  await waitForNextUpdate()
+  ;[debouncedValue, isDebouncing] = result.current
+  expect(debouncedValue).toEqual('newValue')
+  expect(isDebouncing).toEqual(false)
+})
