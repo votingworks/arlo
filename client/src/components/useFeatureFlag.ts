768d94d267f22be79bffd770762e54f519142d13
Jonah Kagan <jonah@voting.works>
2022-09-28 15:21:09 -0700

    Enable batch inventory flow for Verified Voting Sandbox

--- client/src/components/useFeatureFlag.ts
+++ client/src/components/useFeatureFlag.ts
@@ -6,6 +6,7 @@ const ENABLE_BATCH_INVENTORY_ORGANIZATION_IDS = [
   'a67791e3-90a0-4d4e-a5e7-929f82bf4ce6', // VotingWorks Internal Sandbox
   'b45800ff-a239-42b3-b285-414cb94d2b6b', // Ginny's Sandbox
   'b7b99303-b1ac-4b52-8a02-22c10846cff3', // Audit Specialist Sandbox
+  'e348fcfd-bd23-4b96-a003-6c3a79abd240', // Verified Voting Sandbox
 ]
 
 // eslint-disable-next-line import/prefer-default-export
