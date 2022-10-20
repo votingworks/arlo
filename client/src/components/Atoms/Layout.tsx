c496a990a03c15c63f5e8557c8245fddb5da7dd8
Jonah Kagan <jonah@voting.works>
2022-10-20 10:05:52 -0700

    Add some basic layout atoms

---
+++ client/src/components/Atoms/Layout.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const CenterRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`
+
+export const CenterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ButtonRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`
