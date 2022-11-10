551d8c4e223672ca1abfb502dba1127c3c45c5ef
arsalansufi <arsalan@voting.works>
2022-11-10 13:38:33 -0500

    Update Hart CVR parsing to support multiple CVR ZIP files (#1675)
    * Update Hart CVR parsing to support multiple CVR ZIP files
    
    One for each tabulator
    
    For Orange County but also others down the road who:
    - Don't have unique batch names across tabulators
    - Don't have tabulator info in their XML files
    - Can easily export CVRs by tabulator
    
    * Update copy to note support for multiple Hart CVR ZIP files
    
    * Update server-side tests
    
    * Update client-side tests
    
    * Remove extra copy for Hart
    
    We'll soon be adding a link to our docs instead
    
    * Clean up error messages
    
    * Add parens to make code more readable
    
    * Add missing space to error message
    
    * One more error message tweak for clarity

--- server/tests/ballot_comparison/snapshots/snap_test_cvrs.py
+++ server/tests/ballot_comparison/snapshots/snap_test_cvrs.py
@@ -997,6 +997,128 @@ snapshots["test_hart_cvr_upload 2"] = {
     },
 }
 
+snapshots["test_hart_cvr_upload_with_multiple_cvr_zip_files 1"] = [
+    {
+        "ballot_position": 1,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-1-1",
+        "interpretations": "0,1,1,0,0,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 2,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-1-2",
+        "interpretations": "1,0,1,0,0,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 3,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-1-3",
+        "interpretations": "0,1,1,0,0,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 1,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-2-1",
+        "interpretations": "1,0,1,0,0,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 2,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-2-2",
+        "interpretations": "0,1,0,1,0,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 3,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-2-3",
+        "interpretations": "1,0,0,0,1,0",
+        "tabulator": "TABULATOR1",
+    },
+    {
+        "ballot_position": 1,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-3-1",
+        "interpretations": "1,0,0,1,0,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 2,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-3-2",
+        "interpretations": "1,0,0,0,1,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 3,
+        "batch_name": "BATCH1",
+        "imprinted_id": "1-3-3",
+        "interpretations": "1,0,0,1,0,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 1,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-4-1",
+        "interpretations": "1,0,0,0,1,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 2,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-4-2",
+        "interpretations": "1,1,1,1,1,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 3,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-4-4",
+        "interpretations": ",,1,0,0,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 4,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-4-5",
+        "interpretations": ",,1,0,0,0",
+        "tabulator": "TABULATOR2",
+    },
+    {
+        "ballot_position": 5,
+        "batch_name": "BATCH2",
+        "imprinted_id": "1-4-6",
+        "interpretations": ",,0,0,0,1",
+        "tabulator": "TABULATOR2",
+    },
+]
+
+snapshots["test_hart_cvr_upload_with_multiple_cvr_zip_files 2"] = {
+    "Contest 1": {
+        "choices": {
+            "Choice 1-1": {"column": 0, "num_votes": 7},
+            "Choice 1-2": {"column": 1, "num_votes": 3},
+        },
+        "total_ballots_cast": 11,
+        "votes_allowed": 1,
+    },
+    "Contest 2": {
+        "choices": {
+            "Choice 2-1": {"column": 2, "num_votes": 6},
+            "Choice 2-2": {"column": 3, "num_votes": 3},
+            "Choice 2-3": {"column": 4, "num_votes": 3},
+            "Write-In": {"column": 5, "num_votes": 1},
+        },
+        "total_ballots_cast": 14,
+        "votes_allowed": 1,
+    },
+}
+
 snapshots["test_hart_cvr_upload_with_scanned_ballot_information 1"] = [
     {
         "ballot_position": 1,
