4f7780bfb4dc93a6f36b8b98e51ffae395936a5d
Chris Combs <ccombs@squarespace.com>
2022-11-15 16:58:09 +0000

    use the local system if no aws keys are configured for cypress tests.

--- client/run-cypress-tests.sh
+++ client/run-cypress-tests.sh
@@ -4,7 +4,10 @@ export ARLO_SMTP_HOST=localhost
 export ARLO_SMTP_PORT=1025
 export ARLO_SMTP_USERNAME=cypress-smtp-username
 export ARLO_SMTP_PASSWORD=cypress-smtp-password
-export ARLO_FILE_UPLOAD_STORAGE_PATH=s3://arlo-file-uploads-dev
+if [[ -n $AWS_ACCESS_KEY_ID ]] && [[ -n $AWS_SECRET_ACCESS_KEY ]]
+then
+    export ARLO_FILE_UPLOAD_STORAGE_PATH=s3://arlo-file-uploads-dev
+fi
 
 trap 'kill 0' SIGINT SIGHUP EXIT
 cd "$(dirname "${BASH_SOURCE[0]}")"
