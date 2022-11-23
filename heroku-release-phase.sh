49b3ae2969c37a5d5682722a5eaaf1303b23b0e1
Matt Bernhard <mdb92nc@gmail.com>
2022-11-23 18:38:14 +0000

    tweaking python call...

--- heroku-release-phase.sh
+++ heroku-release-phase.sh
@@ -6,7 +6,7 @@ if [[ $DATABASE_URL ]]; then
 
     # Reset the db if we're deploying the vuln scanning app
     if [[ $SCANNING_INSTANCE ]]; then
-        poetry run python -m scripts.resetdb
+        python -m scripts.resetdb
     fi
 else
     echo "DATABASE_URL not set, skipping migrations"
