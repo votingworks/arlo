cf4c5d9f093256cb6544380732ac42b345f9f328
Matt Bernhard <mdb92nc@gmail.com>
2022-11-28 20:47:50 +0000

    reverting release script

--- heroku-release-phase.sh
+++ heroku-release-phase.sh
@@ -3,11 +3,6 @@
 echo "Attempting to run migrations"
 if [[ $DATABASE_URL ]]; then
     alembic upgrade head
-
-    # Reset the db if we're deploying the vuln scanning app
-    if [[ $SCANNING_INSTANCE ]]; then
-       ~/.local/bin/poetry python -m scripts.resetdb
-    fi
 else
     echo "DATABASE_URL not set, skipping migrations"
 fi
