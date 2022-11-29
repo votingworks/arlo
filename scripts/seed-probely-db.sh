eeee1d3acd8d25d9d8f553286c8ad9015990d42d
Matt Bernhard <mdb92nc@gmail.com>
2022-11-29 20:55:19 +0000

    modifying restore script to work on staging

---
+++ scripts/seed-probely-db.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+BACKUP=${1:-latest.dump}
+export FLASK_ENV=${FLASK_ENV:-development}
+dropdb -U arlo arlo
+createdb -U arlo arlo
+pg_restore --verbose --clean --no-acl --no-owner -U arlo -d arlo $BACKUP
