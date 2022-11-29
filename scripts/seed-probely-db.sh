cde106579ebd4a206ffc3859c94bac35694bf5f4
Matt Bernhard <mdb92nc@gmail.com>
2022-11-29 21:39:52 +0000

    Working on seed script

--- scripts/seed-probely-db.sh
+++ scripts/seed-probely-db.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 BACKUP=${1:-latest.dump}
-export FLASK_ENV=${FLASK_ENV:-development}
-dropdb -U arlo arlo
-createdb -U arlo arlo
+python -m scripts.resetdb
 pg_restore --verbose --clean --no-acl --no-owner -U arlo -d $DATABASE_URL $BACKUP
