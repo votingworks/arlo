f672708e4666e6058a3bec1adf6e35e87406fcf2
Chris Combs <combsc@umich.edu>
2022-12-27 03:05:33 +0000

    Update minerva2 test

--- server/tests/audit_math/test_minerva2.py
+++ server/tests/audit_math/test_minerva2.py
@@ -1,5 +1,6 @@
 from r2b2.minerva2 import Minerva2
 from r2b2.contest import Contest as rContest, ContestType
+from pytest import approx
 
 from ...audit_math import minerva2, minerva
 
@@ -31,24 +32,43 @@ def test_get_sample_size():
     )
 
 
-# TODO: IDEA
-# Look into how these functions are called (get_sample_size and compute_risk). I
-# suspect that when someone's running arlo, they're pretty much just running one
-# election at a time. If that's the case, it should be reasonable to have
-# minerva2.py track existing elections and pick up where they were left off.
-# We shouldn't have too many minerva2 instances running around in memory, and if
-# we do we can just get rid of them and remake them later? Need to think through
-# how this ought to work
+def test_get_sample_size_multi_candidate():
+    arlo1 = minerva.make_arlo_contest({"a": 60000, "b": 40000, "c": 10000})
+    arlo2 = minerva.make_arlo_contest({"a": 51000, "b": 49000, "c": 10000})
+    arlo3 = minerva.make_arlo_contest({"a": 5040799, "b": 10000000 - 5040799, "c": 1000000})
+    assert minerva2.get_sample_size(RISK_LIMIT, arlo1, None, None)["0.9"]["size"] == 191
+    assert (
+        minerva2.get_sample_size(RISK_LIMIT, arlo2, None, None)["0.9"]["size"] == 18997
+    )
+    assert (
+        minerva2.get_sample_size(RISK_LIMIT, arlo3, None, None)["0.9"]["size"] == 113814
+    )
 
+def test_get_sample_size_2win():
+    d2 = minerva.make_arlo_contest(
+        {"a": 400, "b": 400, "c": 200, "d": 100}, num_winners=2
+    )
+    res = minerva2.get_sample_size(10, d2, None, [])
+    assert res == {
+        "0.7": {"prob": 0.7, "size": 76, "type": None},
+        "0.8": {"prob": 0.8, "size": 87, "type": None},
+        "0.9": {"prob": 0.9, "size": 118, "type": None},
+    }
+    sample_results = {"1": {"a": 30, "b": 30, "c": 20, "d": 20}}
+    round_schedule = {1: ("1", 100)}
+    res = minerva2.get_sample_size(10, d2, sample_results, round_schedule)
+    assert res == {
+        "0.7": {"prob": 0.7, "size": 297, "type": None},
+        "0.8": {"prob": 0.8, "size": 315, "type": None},
+        "0.9": {"prob": 0.9, "size": 319, "type": None},
+    }
 
 def test_get_sample_size_multiple_rounds():
     contest1 = rContest(
         100000, {"A": 60000, "B": 40000}, 1, ["A"], ContestType.MAJORITY
     )
     minerva1 = Minerva2(ALPHA, 1.0, contest1)
-    minerva1.compute_min_winner_ballots(minerva1.sub_audits["A-B"], 100)
-    minerva1.sample_ballots["A"].append(54)
-    minerva1.sample_ballots["B"].append(100 - 54)
+    minerva1.execute_round(100, {"A": 54, "B": 46})
     r2b2_result = minerva1.next_sample_size()
     arlo = minerva.make_arlo_contest({"A": 60000, "B": 40000})
     sample_results = {"1": {"A": 54, "B": 100 - 54}}
@@ -58,16 +78,15 @@ def test_get_sample_size_multiple_rounds():
     )["0.9"]["size"]
     assert r2b2_result == arlo_result
 
-    minerva1.compute_min_winner_ballots(minerva1.sub_audits["A-B"], 200)
-    minerva1.sample_ballots["A"].append(113)
-    minerva1.sample_ballots["B"].append(200 - 113)
+    minerva1.execute_round(200, {"A": 113, "B": 200 - 113})
     r2b2_result = minerva1.next_sample_size()
-    sample_results['2'] = {"A": 113, "B": 200 - 113}
-    round_schedule[2] = ("2", 200)
+    sample_results['2'] = {"A": 113 - 54, "B": 200 - 113 - (100 - 54)}
+    round_schedule[2] = ("2", 100)
     arlo_result = minerva2.get_sample_size(
         RISK_LIMIT, arlo, sample_results, round_schedule
     )["0.9"]["size"]
     assert r2b2_result == arlo_result
+    #TODO: Figure out why this is broken hehe
 
 
 def test_compute_risk():
@@ -81,3 +100,32 @@ def test_compute_risk():
     round_schedule = {1: ("1", 100)}
     risk = minerva2.compute_risk(RISK_LIMIT, arlo, sample_results, round_schedule)
     assert minerva1.get_risk_level() == risk[0][("winner", "loser")]
+
+def test_compute_risk_2win():
+    c = minerva.make_arlo_contest(
+        {"a": 400, "b": 400, "c": 200, "d": 100}, num_winners=2
+    )
+    res = minerva2.compute_risk(
+        10, c, minerva.make_sample_results(c, [[40, 40, 18, 2]]), {1: ("r0", 100)}
+    )
+    assert res == ({("winner", "loser"): approx(0.0064653703790821795)}, True)
+    res = minerva2.compute_risk(
+        10, c, minerva.make_sample_results(c, [[30, 30, 30, 10]]), {1: ("r0", 100)}
+    )
+    assert res == ({("winner", "loser"): approx(0.552702598296842)}, False)
+
+def test_compute_risk_multi_round():
+    c = minerva.make_arlo_contest(
+        {"A": 450, "B": 400}
+    )
+    sample_results = {"1": {"A": 54, "B": 100 - 54}}
+    round_schedule = {1: ("1", 100)}
+    sample_results["2"] = {"A": 50, "B": 100 - 50} 
+    round_schedule[2] = ("2", 100)
+    res = minerva2.compute_risk(
+        10,
+        c,
+        sample_results,
+        round_schedule
+    )
+    assert res == ({("winner", "loser"): approx(0.083535346859948)}, True)
