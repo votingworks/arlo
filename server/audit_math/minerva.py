ca70966a4ed9d485d5966554af321976312019dd
Chris Combs <combsc@umich.edu>
2023-01-02 15:56:33 +0000

    format server

--- server/audit_math/minerva.py
+++ server/audit_math/minerva.py
@@ -15,7 +15,7 @@ from typing import List, Dict, Tuple, Optional
 
 from athena.audit import Audit as AthenaAudit  # type: ignore
 from .sampler_contest import Contest
-from .ballot_polling_types import ( # pylint: disable=unused-import
+from .ballot_polling_types import (  # pylint: disable=unused-import
     SampleSizeOption,
     BALLOT_POLLING_ROUND_SIZES,
     BALLOT_POLLING_SAMPLE_RESULTS,
