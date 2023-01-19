95182960f5571ffcb0698b447d93caa6494b9efd
Chris Combs <combsc@umich.edu>
2023-01-19 14:35:07 +0000

    minerva 2 -> providence

--- client/src/components/HomeScreen.tsx
+++ client/src/components/HomeScreen.tsx
@@ -517,7 +517,7 @@ const CreateAudit = ({ organizations }: { organizations: IOrganization[] }) => {
                         <Radio value="BRAVO">BRAVO</Radio>
                         <Radio value="MINERVA">Minerva (Not recommended)</Radio>
                         <Radio value="PROVIDENCE">
-                          Minerva 2 (Not recommended)
+                          Providence (Not recommended)
                         </Radio>
                       </RadioGroup>
                     </label>
