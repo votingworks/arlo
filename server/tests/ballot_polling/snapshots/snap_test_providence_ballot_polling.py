31318eba3fb790d37ba993d8a71a0e3b31e7eb06
Chris Combs <combsc@umich.edu>
2023-01-19 14:38:23 +0000

    format server

--- server/tests/ballot_polling/snapshots/snap_test_providence_ballot_polling.py
+++ server/tests/ballot_polling/snapshots/snap_test_providence_ballot_polling.py
@@ -7,20 +7,8 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_providence_sample_size 1'] = [
-    {
-        'key': '0.7',
-        'prob': 0.7,
-        'size': 106
-    },
-    {
-        'key': '0.8',
-        'prob': 0.8,
-        'size': 136
-    },
-    {
-        'key': '0.9',
-        'prob': 0.9,
-        'size': 173
-    }
+snapshots["test_providence_sample_size 1"] = [
+    {"key": "0.7", "prob": 0.7, "size": 106},
+    {"key": "0.8", "prob": 0.8, "size": 136},
+    {"key": "0.9", "prob": 0.9, "size": 173},
 ]
