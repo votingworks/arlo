6c214a257953f7df0e5798995fb8645536651cdf
Jonah Kagan <jonah@voting.works>
2023-09-12 23:56:18 +0000

    Resolve some outstanding lint warnings

--- client/src/components/Atoms/StatusBox.test.tsx
+++ client/src/components/Atoms/StatusBox.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, useParams } from 'react-router-dom'
-import { render, fireEvent, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { QueryClientProvider } from 'react-query'
 import { ToastContainer } from 'react-toastify'
