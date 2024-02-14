9258bf20073ba05522a83f8c5e4d5cbe547ba7b8
arsalansufi <arsalan@voting.works>
2024-02-14 16:43:59 -0500

    Disable extra batch selection for Georgia, for now (#1880)

--- server/feature_flags.py
+++ server/feature_flags.py
@@ -3,6 +3,5 @@ from .models import Election
 
 def is_enabled_sample_extra_batches_by_counting_group(election: Election):
     return election.organization_id in [
-        "b216ad0d-1481-44e4-a2c1-95da40175084",  # Georgia
         "test_org_sample_extra_batches_by_counting_group",  # For tests
     ]
