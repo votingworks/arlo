a8ed9ed50e0f1404de0d5874ec3d818a58b4a624
Jonah Kagan <jonah@voting.works>
2024-10-28 12:53:13 -0700

    Fix db migration merge conflict

--- server/migrations/versions/6c256e8152f8_backgroundtask_worker_id.py
+++ server/migrations/versions/6c256e8152f8_backgroundtask_worker_id.py
@@ -2,7 +2,7 @@
 """BackgroundTask.worker_id
 
 Revision ID: 6c256e8152f8
-Revises: 8452f909a07e
+Revises: 34824a2d1ba8
 Create Date: 2024-10-23 23:34:52.713596+00:00
 
 """
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "6c256e8152f8"
-down_revision = "8452f909a07e"
+down_revision = "34824a2d1ba8"
 branch_labels = None
 depends_on = None
 
