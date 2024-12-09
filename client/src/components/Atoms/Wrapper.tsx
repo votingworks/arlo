e1a38d536b2f27b0c312c991ecaa6d2539b55ec0
Nikhil Bhatia <nikhil@voting.works>
2024-12-09 19:34:36 +0000

    Keep other support tool pages a similar width

--- client/src/components/Atoms/Wrapper.tsx
+++ client/src/components/Atoms/Wrapper.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 85%;
+  width: 100%;
   padding-bottom: 30px;
 `
 
@@ -17,9 +17,19 @@ export const Inner = styled.div<IInnerProps>`
   margin-right: auto;
   margin-left: auto;
   width: 100%;
+  max-width: 1020px;
   padding: 0 30px;
   padding-top: ${props => (props.withTopPadding ? '30px' : undefined)};
   flex-direction: ${props => props.flexDirection || undefined};
+`
+
+export const SupportToolsInner = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 30px 0;
+  width: 80%;
 `
 
 export default Wrapper
