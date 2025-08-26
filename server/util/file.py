2de57fc78872c3745ce453da5058d007f4c9d4e2
Arsalan Sufi <arsalan@voting.works>
2025-08-26 09:39:46 -0400

    Filter out hidden files before performing expected file check

--- server/util/file.py
+++ server/util/file.py
@@ -124,7 +124,12 @@ def zip_files(files: Mapping[str, IO[bytes]]) -> IO[bytes]:
 
 def read_zip_filenames(zip_file: BinaryIO) -> List[str]:
     with ZipFile(zip_file, "r") as zip_archive:
-        return zip_archive.namelist()
+        return [
+            entry_name
+            for entry_name in zip_archive.namelist()
+            # ZIP files created on Macs include a hidden __MACOSX folder
+            if not entry_name.startswith("__") and not entry_name.startswith(".")
+        ]
 
 
 # Extracts the contents of the provided zip file to the specified directory and returns the list of
