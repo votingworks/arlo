36ef2dc72e38a820b7d9a9e02792f07d479ff184
Nikhil Bhatia <nikhil@voting.works>
2026-06-02 22:27:26 +0000

    Update snapshots

--- server/tests/batch_comparison/snapshots/snap_test_sample_extra_batches_by_counting_group.py
+++ server/tests/batch_comparison/snapshots/snap_test_sample_extra_batches_by_counting_group.py
@@ -58,11 +58,8 @@ Round Number,Contest Name,Targeted?,Sample Size,Risk Limit Met?,P-Value,Start Ti
 \r
 ######## SAMPLED BATCHES ########\r
 Jurisdiction Name,Batch Name,Ballots in Batch,Ticket Numbers: Contest 1,Audited?,Reported Results: Contest 1,Audit Results: Contest 1,Change in Results: Contest 1,Change in Margin: Contest 1,Last Edited By,Combined Batch\r
-J1,Batch 7,100,Round 1: EXTRA,Yes,candidate 1: 100; candidate 2: 50; candidate 3: 50,,,,support@example.org,"Combined Batch - Extra, Unsampled"\r
 J1,Batch 9,100,Round 1: EXTRA,Yes,candidate 1: 100; candidate 2: 50; candidate 3: 50,candidate 1: 100; candidate 2: 50; candidate 3: 50,,,support@example.org,\r
-J2,Batch 3,500,Round 1: 0.368061935896261076,Yes,candidate 1: 500; candidate 2: 250; candidate 3: 250,,,,support@example.org,"Combined Batch - Sampled, Extra, Unsampled"\r
-J2,Batch 6,250,Round 1: EXTRA,Yes,candidate 1: 200; candidate 2: 150; candidate 3: 150,,,,support@example.org,"Combined Batch - Sampled, Extra, Unsampled"\r
-J1,"Combined Batch - Extra, Unsampled",600,,Yes,candidate 1: 600; candidate 2: 300; candidate 3: 300,candidate 1: 600; candidate 2: 300; candidate 3: 300,,,support@example.org,"Combines Batch 1, Batch 7"\r
-J2,"Combined Batch - Sampled, Extra, Unsampled",1250,,Yes,candidate 1: 1200; candidate 2: 650; candidate 3: 650,candidate 1: 1200; candidate 2: 650; candidate 3: 650,,,support@example.org,"Combines Batch 1, Batch 3, Batch 6"\r
+J1,"Combined Batch - Extra, Unsampled",600,Round 1: EXTRA (Batch 7),Yes,candidate 1: 600; candidate 2: 300; candidate 3: 300,candidate 1: 600; candidate 2: 300; candidate 3: 300,,,support@example.org,"Combines Batch 1, Batch 7"\r
+J2,"Combined Batch - Sampled, Extra, Unsampled",1250,Round 1: 0.368061935896261076 (Batch 3); Round 1: EXTRA (Batch 6),Yes,candidate 1: 1200; candidate 2: 650; candidate 3: 650,candidate 1: 1200; candidate 2: 650; candidate 3: 650,,,support@example.org,"Combines Batch 1, Batch 3, Batch 6"\r
 Totals,,1950,,,candidate 1: 1900; candidate 2: 1000; candidate 3: 1000,candidate 1: 1900; candidate 2: 1000; candidate 3: 1000,,\r
 """
